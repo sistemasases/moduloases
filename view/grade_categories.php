39bfb53929b34a39efc2dea53ddb2605523cdb48
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-22 06:23:30 -0500

    Arreglos grade_categories

--- view/grade_categories.php
+++ view/grade_categories.php
@@ -24,16 +24,18 @@
  * @copyright  2017 Camilo José Cruz Rivera <cruz.camilo@correounivalle.edu.co>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
+
 // Standard GPL and phpdocs
 require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once('../managers/instance_management/instance_lib.php');
 require_once('../managers/grade_categories/grade_categories_lib.php');
+require_once('../managers/lib/lib.php');
 include('../lib.php');
 include("../classes/output/renderer.php");
 
 global $PAGE;
+global $USER;
 
 $title = "LISTADO DOCENTES";
 $pagetitle = $title;
@@ -77,69 +79,83 @@ $PAGE->requires->css('/blocks/ases/js/DataTables-1.10.12/css/jquery.dataTables_t
 $PAGE->requires->css('/blocks/ases/js/DataTables-1.10.12/css/dataTables.tableTools.css', true);
 $PAGE->requires->css('/blocks/ases/js/DataTables-1.10.12/css/NewCSSExport/buttons.dataTables.min.css', true);
 $PAGE->requires->css('/blocks/ases/js/DataTables-1.10.12/css/dataTables.tableTools.css', true);
-//$PAGE->requires->css('/theme/base/style/core.css',true);
 $PAGE->requires->css('/blocks/ases/style/sweetalert.css', true);
-// $PAGE->requires->js('/blocks/ases/js/grade_categories.js', true);
-// $PAGE->requires->js('/blocks/ases/js/jquery-2.2.4.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/jquery.dataTables.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/jquery.dataTables.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/dataTables.jqueryui.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/dataTables.bootstrap.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/dataTables.bootstrap.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/dataTables.tableTools.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/dataTables.tableTools.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/buttons.flash.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/buttons.html5.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/buttons.print.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/dataTables.buttons.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/jszip.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/pdfmake.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/DataTables-1.10.12/js/NewJSExport/vfs_fonts.js', true);
-// // $PAGE->requires->js('/blocks/ases/js/attendance_table.js', true);
-// $PAGE->requires->js('/blocks/ases/js/main.js', true);
-// $PAGE->requires->js('/blocks/ases/js/checkrole.js', true);
-// $PAGE->requires->js('/blocks/ases/js/bootstrap.js', true);
-// $PAGE->requires->js('/blocks/ases/js/bootstrap.min.js', true);
-// $PAGE->requires->js('/blocks/ases/js/sweetalert-dev.js', true);
-// $PAGE->requires->js('/blocks/ases/js/grade_categories.js', true);
+
 $output = $PAGE->get_renderer('block_ases');
 
 
 //Carga de informacion a mostrar
+$rol = get_role_ases($USER->id);
 $courses = get_courses_pilos($blockid);
 
-$htmlTable =  "<table id = 'teachers' cellspacing='0' width='100%' border>
-                <thead> 
-                    <tr>
-                        <th style = 'border-right: none'> Nombre Completo </th>
-                        <th style='border-left: none; width: 60px;'>  </th>
-                    </tr>
-                </thead><tbody>
-                ";
-$index = 0;
-$courses_info = '';
-foreach($courses as $profe => $cursos){
-    $htmlTable.="<tr class='cerrado' id = 'profesor_$index' ><td style='border-right: none' id = 'profe_$index'>$profe</td><td style='border-left: none'><button style='width: 100%' class = 'desplegate'><i class='glyphicon glyphicon-chevron-left'></i></button></td></tr>";
+if($rol == 'sistemas'){
     
-    $courses_info.="<div style = 'padding-left: 15px' id = 'curso_$index'>";
-    foreach ($cursos as $curso) {
-        $id = $curso['id'];
-        $name = $curso['nombre'];
-        $shortname = $curso['shortname'];
-        $courses_info.="<div class = 'ir_curso' id='$id'> <i class = 'glyphicon glyphicon-new-window'></i> $name $shortname </div>";
+    $htmlTable =  "<table id = 'teachers' cellspacing='0' width='100%' border>
+                    <thead>
+                        <tr>
+                            <th style = 'border-right: none'> Nombre Completo </th>
+                            <th style='border-left: none; width: 60px;'>  </th>
+                        </tr>
+                    </thead><tbody>
+                    ";
+    $index = 0;
+    $courses_info = '';
+    foreach($courses as $profe => $cursos){
+        $htmlTable.="<tr class='cerrado' id = 'profesor_$index' ><td style='border-right: none' id = 'profe_$index'>$profe</td><td style='border-left: none'><button style='width: 100%' class = 'desplegate'><i class='glyphicon glyphicon-chevron-left'></i></button></td></tr>";
+    
+        $courses_info.="<div style = 'padding-left: 15px' id = 'curso_$index'>";
+        foreach ($cursos as $curso) {
+            $id = $curso['id'];
+            $name = $curso['nombre'];
+            $shortname = $curso['shortname'];
+            $courses_info.="<div class = 'ir_curso' id='$id'> <i class = 'glyphicon glyphicon-new-window'></i> $name $shortname </div>";
+        }
+        $courses_info.="</div>";
+        $index = $index+1;
     }
-    $courses_info.="</div>";
-    $index = $index+1;
+    
+    $htmlTable.='</tbody></table>';
+}else{
+    $user_name = "$USER->firstname $USER->lastname";
+    $htmlTable =  "<table id = 'teachers' cellspacing='0' width='100%' border>
+                    <thead>
+                        <tr>
+                            <th style = 'border-right: none'> Nombre Completo $user_name </th>
+                            <th style='border-left: none; width: 60px;'>  </th>
+                        </tr>
+                    </thead><tbody>
+                    ";
+    $index = 0;
+    $courses_info = '';
+    
+    foreach($courses as $profe => $cursos){
+        
+        if($user_name == $profe){
+            $htmlTable.="<tr class='cerrado' id = 'profesor_$index' ><td style='border-right: none' id = 'profe_$index'>$profe</td><td style='border-left: none'><button style='width: 100%' class = 'desplegate'><i class='glyphicon glyphicon-chevron-left'></i></button></td></tr>";
+            
+            $courses_info.="<div style = 'padding-left: 15px' id = 'curso_$index'>";
+            foreach ($cursos as $curso) {
+                $id = $curso['id'];
+                $name = $curso['nombre'];
+                $shortname = $curso['shortname'];
+                $courses_info.="<div class = 'ir_curso' id='$id'> <i class = 'glyphicon glyphicon-new-window'></i> $name $shortname </div>";
+            }
+            $courses_info.="</div>";
+            $index = $index+1;
+        }     
+    }
+    
+    $htmlTable.='</tbody></table>';
 }
 
-$htmlTable.='</tbody></table>';
+
 
 $record = new stdClass;
 $record->teachersTable = $htmlTable;
 $record->courses_info = $courses_info;
 
 echo $output->header();
-//echo $output->standard_head_html(); 
+//echo $output->standard_head_html();
 $grade_categories_page = new \block_ases\output\grade_categories_page($record);
 echo $output->render($grade_categories_page);
 echo $output->footer();
