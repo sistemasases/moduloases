e57f5fde5681c371d6982ef7b97d9cc2edcb7ffc
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-25 14:35:59 -0500

    Update checkrole.js

--- amd/src/ases_report/checkrole.js
+++ amd/src/ases_report/checkrole.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> db_management
 // // requirejs(['jquery'], function($){
 
 
@@ -1402,6 +1398,3 @@
 //   })
 // }
 // });
->>>>>>> 97c7d23d80c7365c0b40027b0d4abac40b2e33b4
-=======
->>>>>>> db_management
