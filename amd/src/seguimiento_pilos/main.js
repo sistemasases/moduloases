4d9397443720984308d094246a98fb473ea324c7
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-25 14:43:30 -0500

    Update main.js

--- amd/src/seguimiento_pilos/main.js
+++ amd/src/seguimiento_pilos/main.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> db_management
- requirejs(['jquery', 'amd_seguimiento_pilos'], function($) {
+requirejs(['jquery', 'amd_seguimiento_pilos'], function($) {
 
 
  });
-<<<<<<< HEAD
-=======
- requirejs(['jquery', 'amd_seguimiento_pilos'], function($) {
-
-
- });
->>>>>>> 97c7d23d80c7365c0b40027b0d4abac40b2e33b4
-=======
->>>>>>> db_management
- 
