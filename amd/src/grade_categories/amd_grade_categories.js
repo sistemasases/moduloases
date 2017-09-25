03ec87fc774e7641574b1c9d9d6a3544cb88ba41
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-25 14:37:14 -0500

    Update amd_grade_categories.js

--- amd/src/grade_categories/amd_grade_categories.js
+++ amd/src/grade_categories/amd_grade_categories.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> db_management
 requirejs(['jquery', 'bootstrap', 'sweetalert', 'datatables.net', 'datatables.net-buttons', 'buttons.flash', 'jszip'
 , 'pdfmake', 'vfs_fonts', 'buttons.html5', 'buttons.print'], function($) {
 
@@ -34,40 +30,3 @@ requirejs(['jquery', 'bootstrap', 'sweetalert', 'datatables.net', 'datatables.ne
     });
 
 });
-<<<<<<< HEAD
-=======
-requirejs(['jquery', 'bootstrap', 'sweetalert', 'datatables.net', 'datatables.net-buttons', 'buttons.flash', 'jszip'
-, 'pdfmake', 'vfs_fonts', 'buttons.html5', 'buttons.print'], function($) {
-
-    $(document).ready(function() {
-        $("#teachers").DataTable();
-    });
-
-    $(document).on('click', '.desplegate', function() {
-        var parent = $(this).parent().parent();
-        var id = parent.attr('id').split("_")[1];
-        var curso = '#curso_' + id;
-        var profe = '#profe_' + id;
-
-        if (parent.hasClass('cerrado')) {
-            $(this).children().removeClass('glyphicon-chevron-left').addClass('glyphicon-chevron-down');
-            $(curso).appendTo(profe);
-        }
-        else {
-            $(this).children().removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-left');
-            $(curso).appendTo('#courses_info');
-        }
-        parent.toggleClass('cerrado');
-
-    });
-
-    $(document).on('click', '.ir_curso', function() {
-        var id_curso = $(this).attr('id');
-        var url = 'calificador.php' + location.search + '&id_course=' + id_curso;
-        window.open(url, '_blank');
-    });
-
-});
->>>>>>> 97c7d23d80c7365c0b40027b0d4abac40b2e33b4
-=======
->>>>>>> db_management
