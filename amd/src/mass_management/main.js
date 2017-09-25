0712f643aba3b39f13eac0513d5fbc6c5aadc108
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-25 14:38:59 -0500

    Update main.js

--- amd/src/mass_management/main.js
+++ amd/src/mass_management/main.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> db_management
  requirejs(['jquery','mass_role_management'], function($) {
 
 
 
  });
-<<<<<<< HEAD
-=======
- requirejs(['jquery','mass_role_management'], function($) {
-
-
-
- });
->>>>>>> 97c7d23d80c7365c0b40027b0d4abac40b2e33b4
-=======
->>>>>>> db_management
- 
