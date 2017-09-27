7694c7d6f6979af63f421710ad0a6c981abdb898
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-27 09:07:16 -0500

    Update version.php

--- version.php
+++ version.php
@@ -4,11 +4,7 @@
     //$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0
     
     //$plugin->version = 2017011301;  // YYYYMMDDHH (year, month, day, 24-hr time)
-<<<<<<< HEAD
-    $plugin->version = 201709061106;
-    $plugin->requires = 2010112400;
-=======
     // Esto es un comentario de prueba
     $plugin->version = 201709211100;
     $plugin->requires = 2010112400;
->>>>>>> db_management
+
