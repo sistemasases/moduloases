9241923036e3c64819fb59c614f00f8e8456791b
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-27 09:12:10 -0500

    Update load_role.php

--- managers/user_management/load_role.php
+++ managers/user_management/load_role.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require_once(dirname(__FILE__). '/../../../../config.php');
@@ -7,16 +6,4 @@ global $DB;
 
 $sql_query = "SELECT nombre_rol FROM {talentospilos_rol}";
 $result = $DB->get_records_sql($sql_query);
-
-=======
-<?php
-
-require_once(dirname(__FILE__). '/../../../../config.php');
-
-global $DB;
-
-$sql_query = "SELECT nombre_rol FROM {talentospilos_rol}";
-$result = $DB->get_records_sql($sql_query);
-
->>>>>>> db_management
 echo json_encode($result);
