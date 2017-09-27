b6053dd3badefc8df9de245ff99b74a608ed0683
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2017-09-27 09:21:27 -0500

    Update academic_processing_profile.php

--- managers/academic_processing_profile.php
+++ managers/academic_processing_profile.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require_once('query.php');
@@ -12,21 +11,4 @@ if(isset($_POST['dat']) && isset($_POST['user'])){
         echo json_encode($total_semesters);
     }
 }
-
-=======
-<?php
-
-require_once('query.php');
-
-
-if(isset($_POST['dat']) && isset($_POST['user'])){
-    
-    if($_POST['dat'] == 'semesters'){
-        $total_semesters = get_grades_courses_student_semester($_POST['user'], true);
-        
-        echo json_encode($total_semesters);
-    }
-}
-
->>>>>>> db_management
 print_r(get_grades_courses_student_semester(144,true));
