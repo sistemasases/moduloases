1ecfbc9058e8dc4a3134495275716659b67166de
sistemasAses <sistemasases@SistemasAses>
2017-10-03 10:08:24 -0500

    amd modules

--- view/student_profile.php
+++ view/student_profile.php
@@ -638,6 +638,8 @@ $PAGE->requires->css('/blocks/ases/style/sugerenciaspilos.css', true);
 $PAGE->requires->css('/blocks/ases/style/forms_pilos.css', true);
 $PAGE->requires->css('/blocks/ases/style/c3.css', true);
 
+$PAGE->requires->js_call_amd('block_ases/student_profile_main','init');
+
 $output = $PAGE->get_renderer('block_ases');
 
 echo $output->header();
