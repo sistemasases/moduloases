742ef3423e17691c40d111169b92c8ca1d2fc5a0
sistemasAses <sistemasases@SistemasAses>
2017-10-23 11:23:36 -0500

    Testing sweetalert1

--- amd/src/periods_management_main.js
+++ amd/src/periods_management_main.js
@@ -1,3 +1,7 @@
+ /**
+  * @module block_ases/periods_management_main
+  */
+
 define(['jquery', 'block_ases/bootstrap', 'block_ases/datatables.net', 'block_ases/datatables.net-buttons', 'block_ases/buttons.flash', 'block_ases/jszip', 'block_ases/pdfmake', 'block_ases/buttons.html5', 'block_ases/buttons.print', 'block_ases/sweetalert','block_ases/select2', 'block_ases/jqueryui'], function($, bootstrap, datatablesnet, datatablesnetbuttons, buttonsflash, jszip, pdfmake, buttonshtml5, buttonsprint, sweetalert, select2, jqueryui) {
 
 	return {
