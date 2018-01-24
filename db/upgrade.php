fdf58c575a9cf996958b8f7000151c40e789d4d9
Juan Pablo Moreno <moreno.juan@correounivalle.edu.co>
2018-01-24 16:08:09 -0500

    Se cambia el tipo de dato del campo id_programa de la tabla talentospilos_res_estudiante

--- db/upgrade.php
+++ db/upgrade.php
@@ -757,7 +757,7 @@ function xmldb_block_ases_upgrade($oldversion = 0) {
 
     // Define field id_programa to be added to talentospilos_res_estudiante.
     $table = new xmldb_table('talentospilos_res_estudiante');
-    $field = new xmldb_field('id_programa', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null, 'id_resolucion');
+    $field = new xmldb_field('id_programa', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, 'id_resolucion');
 
     // Conditionally launch add field id_programa.
     if (!$dbman->field_exists($table, $field)) {
