8c97ae2620a1892791f3b41fdfb8ed6090431608
vvbv <jeison.cardona@correounivalle.edu.co>
2018-01-25 10:15:41 -0500

    Encapsulado de los renderizadores.

--- managers/dphpforms/dphpforms_forms_core.php
+++ managers/dphpforms/dphpforms_forms_core.php
@@ -2,10 +2,9 @@
 
     require_once(dirname(__FILE__). '/../../../../config.php');
 
-    global $DB;
     //ALPHA
     function dphpforms_render_recorder($id_form, $rol, $id_estudiante, $id_monitor){};
-    function dphpforms_render_updater($id_completed_form, $rol){};
+    function dphpforms_render_updater($id_completed_form, $rol, $record_id){};
 
 
 ?>
