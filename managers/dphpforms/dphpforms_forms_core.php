daa01e10a675f367b7a09cb7e660c2d959719bb0
vvbv <jeison.cardona@correounivalle.edu.co>
2018-01-25 10:18:13 -0500

    + FIX requiere_once para el renderizador.

--- managers/dphpforms/dphpforms_forms_core.php
+++ managers/dphpforms/dphpforms_forms_core.php
@@ -1,10 +1,16 @@
 <?php 
 
     require_once(dirname(__FILE__). '/../../../../config.php');
+    require_once('dphpforms_record_updater.php');
+    require_once('dphpforms_response_recorder.php');
 
     //ALPHA
-    function dphpforms_render_recorder($id_form, $rol, $id_estudiante, $id_monitor){};
-    function dphpforms_render_updater($id_completed_form, $rol, $record_id){};
+    function dphpforms_render_recorder($id_form, $rol, $id_estudiante, $id_monitor){
+        return dphpforms_generate_html_recorder($id_form, $rol, $id_estudiante, $id_monitor);
+    };
+    function dphpforms_render_updater($id_completed_form, $rol, $record_id){
+        return dphpforms_generate_html_updater($id_completed_form, $rol, $record_id);
+    };
 
 
 ?>
