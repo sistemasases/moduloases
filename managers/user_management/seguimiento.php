85dd07ba6cc5e5ac919acfb4fb54eb42ac5ca313
vvbv <jeison.cardona@correounivalle.edu.co>
2018-02-14 10:46:04 -0500

    + Envio de alertas.

--- managers/user_management/seguimiento.php
+++ managers/user_management/seguimiento.php
@@ -32,9 +32,19 @@ require_once('user_functions.php');
 
 require_once('user_lib.php');
 
+$_JSON_POST_INPUT = json_decode(file_get_contents('php://input'));
+
 //Verifies which functions will be executed to call the respective method or returns a json wheter with an email or  array of students, .
-if(isset($_POST['function'])){
-    switch($_POST['function']){
+if(isset($_POST['function']) || !(is_null($_JSON_POST_INPUT)) ){
+    
+    $function = null;
+    if(isset($_POST['function'])){
+        $function = $_POST['function'];
+    }elseif(!(is_null($_JSON_POST_INPUT))){
+        $function = $_JSON_POST_INPUT->function;
+    }
+
+    switch($function){
         case "delete":
               deleteSeg();
               break;
@@ -62,6 +72,9 @@ if(isset($_POST['function'])){
             break;
         case "send_email":
             echo send_email($_POST["risk_array"], $_POST["observations_array"],'' ,$_POST["id_student_moodle"], $_POST["id_student_pilos"], $_POST["date"],'', '', $_POST["url"]);
+            break;
+        case "send_email_dphpforms":
+            echo send_email_dphpforms($_JSON_POST_INPUT->risks, $_JSON_POST_INPUT->student_code, $_JSON_POST_INPUT->date, "", "", $_JSON_POST_INPUT->url );
             break;
         case "students_consult":
             $students = get_students($_POST["instancia"]);
@@ -604,16 +617,16 @@ function send_email($risk_array, $observations_array, $id_receiving_user, $id_st
     return $email_result;
 }
 
-function send_email_dphpforms($json_risk_observation, $id_receiving_user, $student_code, $date, $subject="", $messageText="", $track_url){
+function send_email_dphpforms($json_risk_observation_, $student_code, $date, $subject="", $messageText="", $track_url){
 
     global $USER, $DB;
 
-    $obj_json_risk_observation = array_values( json_decode($json_risk_observation) );
+    $json_risk_observation = array_values( $json_risk_observation_ );
 
     $emailToUser = new stdClass;
     $emailFromUser = new stdClass;
 
-    $id_moodle = get_id_user_moodle(get_ases_user_by_code($student_code));
+    $id_moodle = get_id_user_moodle( get_ases_user_by_code($student_code)->id );
 
     $id_professional = get_id_assigned_professional($id_moodle);
     $id_practicante = get_id_assigned_pract($id_moodle);
@@ -669,27 +682,33 @@ function send_email_dphpforms($json_risk_observation, $id_receiving_user, $stude
     $messageHtml .= "<b>Código:</b> $student_info->username <br>";
     $messageHtml .= "<b>Correo electrónico:</b> $student_info->email <br><br>";
 
-    if(count($obj_json_risk_observation) > 1){
+    $risk_lvl3_counter = 0;
+    $riskMessage = '';
+
+    foreach($json_risk_observation as $key => $risk){
+
+        if($risk->risk_lvl == 3){
+            $riskMessage .= "<li>";    
+            $riskMessage .= "<b>".$risk->name."</b><br>";
+            $riskMessage .= $risk->observation."<br>";
+            $riskMessage .= "</li>";
+            $risk_lvl3_counter++;
+        }
+
+    }
+
+    if($risk_lvl3_counter > 1){
         $messageHtml .= "En los componentes: <br><br>";
         $messageHtml .= "<ul>";
-        for($i = 0; $i < count($risk_array); $i++){
-
-            foreach($obj_json_risk_observation as $key => $value){
-                /*$messageHtml .= "<li>";    
-                $messageHtml .= "<b>".$risk_array[$i]."</b><br>";
-                $messageHtml .= $observations_array[$i]."<br>";
-                $messageHtml .= "</li>";    */
-            }
-        }
-        $messageHtml .= "</ul>";
     }else{
-        $messageHtml .= "En el componente: ";
-        /*$messageHtml .= "<li>";
-        $messageHtml .= $risk_array[0]."<br>";
-        $messageHtml .= $observations_array[0]."<br>";
-        $messageHtml .= "</li>";*/
-        $messageHtml .= "</ul>";
+        $messageHtml .= "En el componente: <br><br>";
+        $messageHtml .= "<ul>";
     }
+
+    $messageHtml .= $riskMessage;
+
+    $messageHtml .= "</ul>";
+    
     
     $messageHtml .= "Fecha de seguimiento: $date <br>";
     $messageHtml .= "El registro fue realizado por: <b>$USER->firstname $USER->lastname</b><br><br>";
@@ -700,6 +719,4 @@ function send_email_dphpforms($json_risk_observation, $id_receiving_user, $stude
     
     return $email_result;
 }
-
-// send_email();
 ?>
