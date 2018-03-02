d0a5cfd9d82d1de325be5ceb9067526f62aff35b
vvbv <jeison.cardona@correounivalle.edu.co>
2018-03-02 16:00:39 -0500

    + Limpieza

--- view/student_profile.php
+++ view/student_profile.php
@@ -828,7 +828,6 @@ if ($student_code != 0) {
 
 // End of data obtaining for risks graphs
 
-    //Pruebas
     $record->form_seguimientos = null;
     $record->primer_acercamiento = null;
     $record->form_seguimientos = dphpforms_render_recorder('seguimiento_pares', $rol);
@@ -836,9 +835,8 @@ if ($student_code != 0) {
     if ($record->form_seguimientos == '') {
         $record->form_seguimientos = "<strong><h3>Oops!: No se ha encontrado un formulario con el alias: <code>seguimiento_pares</code>.</h3></strong>";
     }
-    $record->has_form =1;
-    $record->primer_acercamiento = dphpforms_render_recorder('primer_acercamiento', $rol);
     
+    $record->primer_acercamiento = dphpforms_render_recorder('primer_acercamiento', $rol);
     if ($record->primer_acercamiento == '') {
         $record->primer_acercamiento = "<strong><h3>Oops!: No se ha encontrado un formulario con el alias: <code>primer_acercamiento</code>.</h3></strong>";
     }
