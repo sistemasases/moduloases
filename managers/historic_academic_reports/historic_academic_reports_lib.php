4977400fd483adf35ddc98712a46eb78ae6a1ba5
cjcruzrivera <cruz.camilo@correounivalle.edu.co>
2018-03-05 17:49:38 -0500

    se realiza consulta reporte

--- managers/historic_academic_reports/historic_academic_reports_lib.php
+++ managers/historic_academic_reports/historic_academic_reports_lib.php
@@ -36,65 +36,64 @@ require_once __DIR__ . '/../../../../config.php';
 function get_datatable_array_Students($instance_id)
 {
     $default_students = $columns = array();
-    array_push($columns, array("title"=>"Número de documento", "name"=>"num_doc", "data"=>"num_doc"));  
-    array_push($columns, array("title"=>"Código estudiante", "name"=>"username", "data"=>"username"));
-    array_push($columns, array("title"=>"Nombre(s)", "name"=>"firstname", "data"=>"firstname"));
-    array_push($columns, array("title"=>"Apellido(s)", "name"=>"lastname", "data"=>"lastname"));
-    array_push($columns, array("title"=>"Semestre", "name"=>"semestre", "data"=>"semestre"));
-    array_push($columns, array("title"=>"Cancela", "name"=>"cancel", "data"=>"cancel"));
-    array_push($columns, array("title"=>"Promedio Semestre", "name"=>"promSem", "data"=>"promSem"));        
-    array_push($columns, array("title"=>"Gano Estimulo", "name"=>"estim", "data"=>"estim"));
-    array_push($columns, array("title"=>"Cae en Bajo", "name"=>"bajo", "data"=>"bajo"));
-    array_push($columns, array("title"=>"Promedio Acumulado", "name"=>"promAcum", "data"=>"promAcum"));
-    array_push($columns, array("title"=>"Estimulos", "name"=>"Numestim", "data"=>"estim"));
-    array_push($columns, array("title"=>"Bajos", "name"=>"bajos", "data"=>"bajos"));
-    array_push($columns, array("title"=>"Materias Perdidas", "name"=>"perdidas", "data"=>"perdidas"));
-    array_push($columns, array("title"=>"Programa", "name"=>"programa", "data"=>"programa"));
-    array_push($columns, array("title"=>"Cohorte", "name"=>"cohorte", "data"=>"cohorte"));
-    
-   // $default_students = get_historic_report($instance_id);
+    array_push($columns, array("title" => "Número de documento", "name" => "num_doc", "data" => "num_doc"));
+    array_push($columns, array("title" => "Código estudiante", "name" => "username", "data" => "username"));
+    array_push($columns, array("title" => "Nombre(s)", "name" => "firstname", "data" => "firstname"));
+    array_push($columns, array("title" => "Apellido(s)", "name" => "lastname", "data" => "lastname"));
+    array_push($columns, array("title" => "Semestre", "name" => "semestre", "data" => "semestre"));
+    array_push($columns, array("title" => "Cancela", "name" => "cancel", "data" => "cancel"));
+    array_push($columns, array("title" => "Promedio Semestre", "name" => "promSem", "data" => "promSem"));
+    array_push($columns, array("title" => "Gano Estimulo", "name" => "estim", "data" => "estim"));
+    array_push($columns, array("title" => "Cae en Bajo", "name" => "bajo", "data" => "bajo"));
+    array_push($columns, array("title" => "Promedio Acumulado", "name" => "promAcum", "data" => "promAcum"));
+    array_push($columns, array("title" => "Estimulos", "name" => "Numestim", "data" => "estim"));
+    array_push($columns, array("title" => "Bajos", "name" => "bajos", "data" => "bajos"));
+    array_push($columns, array("title" => "Materias Perdidas", "name" => "perdidas", "data" => "perdidas"));
+    array_push($columns, array("title" => "Programa", "name" => "programa", "data" => "programa"));
+    array_push($columns, array("title" => "Cohorte", "name" => "cohorte", "data" => "cohorte"));
+
+    $default_students = get_historic_report($instance_id);
 
     $data_to_table = array(
         "bsort" => false,
-        "data"=> $default_students,
+        "data" => $default_students,
         "columns" => $columns,
         "select" => "false",
-        "fixedHeader"=> array(
-            "header"=> true,
-            "footer"=> true
+        "fixedHeader" => array(
+            "header" => true,
+            "footer" => true,
         ),
-        "language" => 
-        array(
-            "search"=> "Buscar:",
-            "oPaginate" => array (
-                "sFirst"=>    "Primero",
-                "sLast"=>     "Último",
-                "sNext"=>     "Siguiente",
-                "sPrevious"=> "Anterior"
+        "language" => array(
+            "search" => "Buscar:",
+            "oPaginate" => array(
+                "sFirst" => "Primero",
+                "sLast" => "Último",
+                "sNext" => "Siguiente",
+                "sPrevious" => "Anterior",
             ),
-            "sProcessing"=>     "Procesando...",
-            "sLengthMenu"=>     "Mostrar _MENU_ registros",
-            "sZeroRecords"=>    "No se encontraron resultados",
-            "sEmptyTable"=>     "Ningún dato disponible en esta tabla",
-            "sInfo"=>           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "sInfoEmpty"=>      "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "sInfoFiltered"=>   "(filtrado de un total de _MAX_ registros)",
-            "sInfoPostFix"=>    "",
-            "sSearch"=>         "Buscar:",
-            "sUrl"=>            "",
-            "sInfoThousands"=>  ",",
-            "sLoadingRecords"=> "Cargando...",
-            "oAria"=> array(
-                "sSortAscending"=>  ": Activar para ordenar la columna de manera ascendente",
-                "sSortDescending"=> ": Activar para ordenar la columna de manera descendente"
-            )
+            "sProcessing" => "Procesando...",
+            "sLengthMenu" => "Mostrar _MENU_ registros",
+            "sZeroRecords" => "No se encontraron resultados",
+            "sEmptyTable" => "Ningún dato disponible en esta tabla",
+            "sInfo" => "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty" => "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered" => "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix" => "",
+            "sSearch" => "Buscar:",
+            "sUrl" => "",
+            "sInfoThousands" => ",",
+            "sLoadingRecords" => "Cargando...",
+            "oAria" => array(
+                "sSortAscending" => ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending" => ": Activar para ordenar la columna de manera descendente",
+            ),
         ),
-        "autoFill"=>"true",
-        "dom"=> "lfrtBip"
+        "autoFill" => "true",
+        "dom" => "lfrtBip",
     );
 
-    return $data_to_table;     
-            
+    return $data_to_table;
+
 }
 
 /**
@@ -102,13 +101,86 @@ function get_datatable_array_Students($instance_id)
  * @see get_historic_report($id_instance)
  * @param $id_instance --> id del modulo
  * @return Array --> info historic_academic_report
-*/
+ */
 
-function get_historic_report($id_instance){
-    
+function get_historic_report($id_instance)
+{
+
     global $DB;
+    $array_historic = array();
 
-    
+    $query = "  SELECT     historic.id as id,
+                           num_doc,
+                           substring(username from 1 FOR 7) AS username,
+                           firstname,
+                           lastname,
+                           semestre.nombre    AS semestre,
+                           promedio_semestre  AS promsem,
+                           promedio_acumulado AS promacum,
+                           programa.nombre    AS programa,
+                           'cohorte.NAME'       AS cohorte,
+                           json_materias
+                FROM       {talentospilos_history_academ} historic
+                INNER JOIN {talentospilos_usuario} usuario
+                ON         historic.id_estudiante = usuario.id
+                INNER JOIN {talentospilos_semestre} semestre
+                ON         historic.id_semestre = semestre.id
+                INNER JOIN {talentospilos_programa} programa
+                ON         historic.id_programa = programa.id
+                INNER JOIN {talentospilos_user_extended} user_ext
+                ON         historic.id_estudiante = user_ext.id_ases_user
+                INNER JOIN {user} user_moodle
+                ON         user_ext.id_moodle_user = user_moodle.id ";
+                      
+    $historics = $DB->get_records_sql($query);
+
+    foreach($historics as $historic){
+
+        //validate cancel 
+        $query_cancel = "SELECT * FROM {talentospilos_history_cancel} WHERE id_history = $historic->id ";
+
+        $cancel = $DB->get_record_sql($query_cancel);
+
+        if($cancel){
+            $historic->cancel = $cancel->fecha_cancelacion;
+        }else{
+            $historic->cancel = "NO";
+        }
+
+        //validate estimulo
+        $query_estimulo = "SELECT * FROM {talentospilos_history_estim} WHERE id_history = $historic->id ";
+
+        $estimulo = $DB->get_records_sql($query_estimulo);
+
+        if($estimulo){
+            $historic->estim = $estimulo->puesto_ocupado;
+        }else{
+            $historic->estim = "NO";            
+        }
+
+        //validate bajo
+        $query_bajo = "SELECT * FROM {talentospilos_history_bajo} WHERE id_history = $historic->id ";
+
+        $bajo = $DB->get_records_sql($query_bajo);
+
+        if($bajo){
+            $historic->bajo = $bajo->numero_bajo;
+        }else{
+            $historic->bajo = "NO";            
+        }
+
+        //validate estimulos
+        $historic->Numestim = "NO";            
+
+        //validate bajos
+        $historic->bajos = "NO";            
+
+        //validate materias perdidas
+        $historic->perdidas = "NO";            
+        array_push($array_historic, $historic);
+    }
+
+    return $array_historic;
 }
 
 /**
@@ -122,53 +194,52 @@ function get_historic_report($id_instance){
 function get_datatable_array_totals($instance_id)
 {
     $default_students = $columns = array();
-    array_push($columns, array("title"=>"Cohorte", "name"=>"cohorte", "data"=>"cohorte"));
-    array_push($columns, array("title"=>"Semestre", "name"=>"semestre", "data"=>"semestre"));
-    array_push($columns, array("title"=>"Total Activos", "name"=>"act", "data"=>"act"));
-    array_push($columns, array("title"=>"Total Inactivos", "name"=>"inact", "data"=>"inact"));
-    array_push($columns, array("title"=>"Total Cohorte", "n"=>"total", "data"=>"total"));
-    
-   // $default_students = get_Totals_report($instance_id);
+    array_push($columns, array("title" => "Cohorte", "name" => "cohorte", "data" => "cohorte"));
+    array_push($columns, array("title" => "Semestre", "name" => "semestre", "data" => "semestre"));
+    array_push($columns, array("title" => "Total Activos", "name" => "act", "data" => "act"));
+    array_push($columns, array("title" => "Total Inactivos", "name" => "inact", "data" => "inact"));
+    array_push($columns, array("title" => "Total Cohorte", "n" => "total", "data" => "total"));
+
+    // $default_students = get_Totals_report($instance_id);
 
     $data_to_table = array(
         "bsort" => false,
-        "data"=> $default_students,
+        "data" => $default_students,
         "columns" => $columns,
         "select" => "false",
-        "fixedHeader"=> array(
-            "header"=> true,
-            "footer"=> true
+        "fixedHeader" => array(
+            "header" => true,
+            "footer" => true,
         ),
-        "language" => 
-        array(
-            "search"=> "Buscar:",
-            "oPaginate" => array (
-                "sFirst"=>    "Primero",
-                "sLast"=>     "Último",
-                "sNext"=>     "Siguiente",
-                "sPrevious"=> "Anterior"
+        "language" => array(
+            "search" => "Buscar:",
+            "oPaginate" => array(
+                "sFirst" => "Primero",
+                "sLast" => "Último",
+                "sNext" => "Siguiente",
+                "sPrevious" => "Anterior",
             ),
-            "sProcessing"=>     "Procesando...",
-            "sLengthMenu"=>     "Mostrar _MENU_ registros",
-            "sZeroRecords"=>    "No se encontraron resultados",
-            "sEmptyTable"=>     "Ningún dato disponible en esta tabla",
-            "sInfo"=>           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "sInfoEmpty"=>      "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "sInfoFiltered"=>   "(filtrado de un total de _MAX_ registros)",
-            "sInfoPostFix"=>    "",
-            "sSearch"=>         "Buscar:",
-            "sUrl"=>            "",
-            "sInfoThousands"=>  ",",
-            "sLoadingRecords"=> "Cargando...",
-            "oAria"=> array(
-                "sSortAscending"=>  ": Activar para ordenar la columna de manera ascendente",
-                "sSortDescending"=> ": Activar para ordenar la columna de manera descendente"
-            )
+            "sProcessing" => "Procesando...",
+            "sLengthMenu" => "Mostrar _MENU_ registros",
+            "sZeroRecords" => "No se encontraron resultados",
+            "sEmptyTable" => "Ningún dato disponible en esta tabla",
+            "sInfo" => "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty" => "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered" => "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix" => "",
+            "sSearch" => "Buscar:",
+            "sUrl" => "",
+            "sInfoThousands" => ",",
+            "sLoadingRecords" => "Cargando...",
+            "oAria" => array(
+                "sSortAscending" => ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending" => ": Activar para ordenar la columna de manera descendente",
+            ),
         ),
-        "autoFill"=>"true",
-        "dom"=> "lfrtBip"
+        "autoFill" => "true",
+        "dom" => "lfrtBip",
     );
 
-    return $data_to_table;     
-            
+    return $data_to_table;
+
 }
