aefaab950f841906a960242ddfa0cc33d0c7fff6
vvbv <jeison.cardona@correounivalle.edu.co>
2018-03-21 13:41:08 -0500

    .

--- amd/src/dphpforms_form_renderer.js
+++ amd/src/dphpforms_form_renderer.js
@@ -83,6 +83,12 @@
                             $('.seg_geo_origen').find('input').prop('checked', false );
                         }
                     });
+                    var count_buttons_dphpforms = $('.dphpforms-record .btn-dphpforms-univalle').length;
+                    if( count_buttons_dphpforms == 1 ){
+                        $('.dphpforms-record .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - ( $('.dphpforms-record .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
+                    }else if( count_buttons_dphpforms == 2 ){
+                        $('.dphpforms-record .btn-dphpforms-univalle:eq(0)').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - 72 ) + 'px'  } );
+                    }
                 });
                 
 
@@ -293,9 +299,14 @@
                     $('.primer_acerca_id_estudiante_field').find('input').val( get_student_code() );
                     var creado_por = $('#current_user_id').val();
                     $('.primer_acerca_id_creado_por_field').find('input').val(creado_por);
-                    $("#modal_primer_acercamiento").find('.dphpforms-response .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-response').width()/2) - ( $('.dphpforms-response .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
-                    $("#modal_primer_acercamiento").find('.dphpforms-updater .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - ( $('.dphpforms-updater .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
-                    
+                    $('.dphpforms-response .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-response').width()/2) - ( $('.dphpforms-response .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
+                    $('.dphpforms-updater .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - ( $('.dphpforms-updater .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
+                    var count_buttons_dphpforms = $('.dphpforms-record .btn-dphpforms-univalle').length;
+                    if( count_buttons_dphpforms == 1 ){
+                        $('.dphpforms-record .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - ( $('.dphpforms-record .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
+                    }else if( count_buttons_dphpforms == 2 ){
+                        $('.dphpforms-record .btn-dphpforms-univalle:eq(0)').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - 72 ) + 'px'  } );
+                    }
                 });
 
                 $('#button_add_geographic_track').on('click', function() {
@@ -310,9 +321,12 @@
                         $('.seg_geo_origen').find('input').prop('disabled', true );
                         $('.seg_geo_origen').find('input').prop('checked', false );
                     }
-                    $('.dphpforms-response .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-response').width()/2) - ( $('.dphpforms-response .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
-                    $('.dphpforms-updater .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - ( $('.dphpforms-updater .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
-                    
+                    var count_buttons_dphpforms = $('.dphpforms-record .btn-dphpforms-univalle').length;
+                    if( count_buttons_dphpforms == 1 ){
+                        $('.dphpforms-record .btn-dphpforms-univalle').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - ( $('.dphpforms-record .btn-dphpforms-univalle').outerWidth() /2) ) + 'px'  } );
+                    }else if( count_buttons_dphpforms == 2 ){
+                        $('.dphpforms-record .btn-dphpforms-univalle:eq(0)').css( { 'margin-left' : ( ($('.dphpforms-updater').width()/2) - 72 ) + 'px'  } );
+                    }
                 });
 
                 $('.mymodal-close').click(function(){
