8bce14325043bd28da45fe2e0c43f355283bdb3d
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2018-06-14 12:03:41 -0500

    update registros erroneos y correccion en carga actualiza_user con nuevo modelo de estado del programa

--- update/registros_erroneos_user_extended.php
+++ update/registros_erroneos_user_extended.php
@@ -8,60 +8,54 @@ cuando tracking status este en 0 lo modifica a 1
 
 global $DB;
 
+$registro_modificar = new StdClass;
+$registro_modificar->id = 5019;
+$registro_modificar->program_status = 1;
+$registro_modificar->tracking_status = 1;
+
+$DB->update_record('talentospilos_user_extended', $registro_modificar);
+
+$registro_modificar2 = new StdClass;
+$registro_modificar2->id = 114;
+$registro_modificar2->program_status = 7;
+
+$DB->update_record('talentospilos_user_extended', $registro_modificar2);
+
+$registro_modificar3 = new StdClass;
+$registro_modificar3->id = 154;
+$registro_modificar3->program_status = 6;
+
+$DB->update_record('talentospilos_user_extended', $registro_modificar3);
+
+$registro_modificar4 = new StdClass;
+$registro_modificar4->id = 5021;
+$registro_modificar4->program_status = 1;
+$registro_modificar4->tracking_status = 1;
+
+$DB->update_record('talentospilos_user_extended', $registro_modificar4);
+
+$registro_modificar5 = new StdClass;
+$registro_modificar5->id = 157;
+$registro_modificar5->program_status = 6;
+
+$DB->update_record('talentospilos_user_extended', $registro_modificar5);
+
+$registro_modificar6 = new StdClass;
+$registro_modificar6->id = 5022;
+$registro_modificar6->program_status = 1;
+$registro_modificar6->tracking_status = 1;
+
+$DB->update_record('talentospilos_user_extended', $registro_modificar6);
+
 // $delete = $DB->execute("DELETE FROM {talentospilos_user_extended} WHERE id = 1066");
 $object_to_delete = array();
-$object_to_delete['id'] = 1066;
+$object_to_delete['id'] = 553;
 $DB->delete_records('talentospilos_user_extended',$object_to_delete);
-// print_r($delete);
-$register1 = new StdClass;
-$register2 = new StdClass;
-$register3 = new StdClass;
 
-$register1->id = 389;
-$register2->id = 243;
-$register3->id = 256;
-$register1->id_ases_user = 333;
-$register2->id_ases_user = 120;
-$register3->id_ases_user = 152;
+$object_to_delete2 = array();
+$object_to_delete2['id'] = 1052;
+$DB->delete_records('talentospilos_user_extended',$object_to_delete2);
 
-$DB->update_record('talentospilos_user_extended', $register1);
-$DB->update_record('talentospilos_user_extended', $register2);
-$DB->update_record('talentospilos_user_extended', $register3);
-
-$new_motivo1= new StdClass;
-$new_motivo2= new StdClass;
-$new_estado_icetex= new StdClass;
-$new_estado_icetex2= new StdClass;
-$new_motivo1->descripcion = "Finaliza ciclo básico";
-$new_motivo2->descripcion = "No definido";
-$new_estado_icetex->nombre = "NO APLICA";
-$new_estado_icetex->descripcion = "No aplica, estudiante que no forma parte del icetex.";
-$new_estado_icetex2->nombre = "NO REGISTRA";
-$new_estado_icetex2->descripcion = "No registra estado icetex.";
-
-$validation1 = "SELECT * FROM {talentospilos_motivos} WHERE descripcion = 'Finaliza ciclo básico' LIMIT 1";
-$validation2 = "SELECT * FROM {talentospilos_motivos} WHERE descripcion = 'No definido' LIMIT 1";
-$validation3 = "SELECT * FROM {talentospilos_estados_icetex} WHERE nombre = 'NO APLICA' LIMIT 1";
-$validation4 = "SELECT * FROM {talentospilos_estados_icetex} WHERE nombre = 'NO REGISTRA' LIMIT 1";
-
-$result1 = $DB->get_record_sql($validation1);
-$result2 = $DB->get_record_sql($validation2);
-$result3 = $DB->get_record_sql($validation3);
-$result4 = $DB->get_record_sql($validation4);
-
-if(!$result1){
-    $insert1 = $DB->insert_record('talentospilos_motivos', $new_motivo1, true);
-}
-
-if(!$result2){
-    $insert2 = $DB->insert_record('talentospilos_motivos', $new_motivo2, true);
-}
-
-if(!$result3){
-    $insert3 = $DB->insert_record('talentospilos_estados_icetex', $new_estado_icetex, true);
-}
-
-if(!$result4){
-    $insert4 = $DB->insert_record('talentospilos_estados_icetex', $new_estado_icetex2, true);
-}
-
+$object_to_delete3 = array();
+$object_to_delete3['id'] = 1059;
+$DB->delete_records('talentospilos_user_extended',$object_to_delete3);
