818839898b57c52d9059c24e5066a5b32910ced7
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2018-06-18 11:42:40 -0500

    updates

--- update/registros_erroneos_user_extended.php
+++ update/registros_erroneos_user_extended.php
@@ -3,7 +3,6 @@
 require_once(dirname(__FILE__). '/../../../config.php');
 
 /* Modifica registros en la tabla {talentospilos_user_extended}
-cuando tracking status este en 0 lo modifica a 1
  */
 
 global $DB;
@@ -13,49 +12,166 @@ $registro_modificar->id = 5019;
 $registro_modificar->program_status = 1;
 $registro_modificar->tracking_status = 1;
 
-$DB->update_record('talentospilos_user_extended', $registro_modificar);
-
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar);
+echo '<br>';
 $registro_modificar2 = new StdClass;
 $registro_modificar2->id = 114;
 $registro_modificar2->program_status = 7;
 
-$DB->update_record('talentospilos_user_extended', $registro_modificar2);
-
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar2);
+echo '<br>';
 $registro_modificar3 = new StdClass;
 $registro_modificar3->id = 154;
 $registro_modificar3->program_status = 6;
 
-$DB->update_record('talentospilos_user_extended', $registro_modificar3);
-
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar3);
+echo '<br>';
 $registro_modificar4 = new StdClass;
 $registro_modificar4->id = 5021;
 $registro_modificar4->program_status = 1;
 $registro_modificar4->tracking_status = 1;
 
-$DB->update_record('talentospilos_user_extended', $registro_modificar4);
-
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar4);
+echo '<br>';
 $registro_modificar5 = new StdClass;
 $registro_modificar5->id = 157;
 $registro_modificar5->program_status = 6;
 
-$DB->update_record('talentospilos_user_extended', $registro_modificar5);
-
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar5);
+echo '<br>';
 $registro_modificar6 = new StdClass;
 $registro_modificar6->id = 5022;
 $registro_modificar6->program_status = 1;
 $registro_modificar6->tracking_status = 1;
 
-$DB->update_record('talentospilos_user_extended', $registro_modificar6);
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar6);
+echo '<br>';
+$registro_modificar7 = new StdClass;
+$registro_modificar7->id = 5024;
+$registro_modificar7->program_status = 1;
+$registro_modificar7->tracking_status = 1;
 
-// $delete = $DB->execute("DELETE FROM {talentospilos_user_extended} WHERE id = 1066");
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar7);
+echo '<br>';
+$registro_modificar8 = new StdClass;
+$registro_modificar8->id = 170;
+$registro_modificar8->program_status = 6;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar8);
+echo '<br>';
+$registro_modificar9 = new StdClass;
+$registro_modificar9->id = 5025;
+$registro_modificar9->program_status = 1;
+$registro_modificar9->tracking_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar9);
+echo '<br>';
+$registro_modificar10 = new StdClass;
+$registro_modificar10->id = 679;
+$registro_modificar10->tracking_status = 0;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar10);
+echo '<br>';
+$registro_modificar11 = new StdClass;
+$registro_modificar11->id = 5026;
+$registro_modificar11->program_status = 1;
+$registro_modificar11->tracking_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar11);
+echo '<br>';
+$registro_modificar12 = new StdClass;
+$registro_modificar12->id = 577;
+$registro_modificar12->tracking_status = 0;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar12);
+echo '<br>';
+
+$registro_modificar13 = new StdClass;
+$registro_modificar13->id = 5027;
+$registro_modificar13->program_status = 1;
+$registro_modificar13->tracking_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar13);
+echo '<br>';
+
+$registro_modificar14 = new StdClass;
+$registro_modificar14->id = 653;
+$registro_modificar14->program_status = 1;
+$registro_modificar14->tracking_status = 0;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar14);
+echo '<br>';
+
+$registro_modificar15 = new StdClass;
+$registro_modificar15->id = 5028;
+$registro_modificar15->program_status = 1;
+$registro_modificar15->tracking_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar15);
+echo '<br>';
+
+$registro_modificar16 = new StdClass;
+$registro_modificar16->id = 580;
+$registro_modificar16->tracking_status = 0;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar16);
+echo '<br>';
+
+$registro_modificar17 = new StdClass;
+$registro_modificar17->id = 244;
+$registro_modificar17->program_status = 6;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar17);
+echo '<br>';
+
+$registro_modificar18 = new StdClass;
+$registro_modificar18->id = 5029;
+$registro_modificar18->program_status = 1;
+$registro_modificar18->tracking_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar18);
+echo '<br>';
+
+$registro_modificar19 = new StdClass;
+$registro_modificar19->id = 5030;
+$registro_modificar19->program_status = 1;
+$registro_modificar19->tracking_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar19);
+echo '<br>';
+
+$registro_modificar20 = new StdClass;
+$registro_modificar20->id = 5031;
+$registro_modificar20->tracking_status = 1;
+$registro_modificar20->program_status = 1;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar20);
+echo '<br>';
+
+$registro_modificar20 = new StdClass;
+$registro_modificar20->id = 690;
+$registro_modificar20->tracking_status = 0;
+
+echo $DB->update_record('talentospilos_user_extended', $registro_modificar20);
+echo '<br>';
+// $delete = echo $DB->execute("DELETE FROM {talentospilos_user_extended} WHERE id = 1066");
 $object_to_delete = array();
 $object_to_delete['id'] = 553;
-$DB->delete_records('talentospilos_user_extended',$object_to_delete);
-
+echo $DB->delete_records('talentospilos_user_extended',$object_to_delete);
+echo '<br>';
 $object_to_delete2 = array();
 $object_to_delete2['id'] = 1052;
-$DB->delete_records('talentospilos_user_extended',$object_to_delete2);
-
+echo $DB->delete_records('talentospilos_user_extended',$object_to_delete2);
+echo '<br>';
 $object_to_delete3 = array();
 $object_to_delete3['id'] = 1059;
-$DB->delete_records('talentospilos_user_extended',$object_to_delete3);
+echo $DB->delete_records('talentospilos_user_extended',$object_to_delete3);
+echo '<br>';
+$object_to_delete4 = array();
+$object_to_delete4['id'] = 1054;
+echo $DB->delete_records('talentospilos_user_extended',$object_to_delete4);
+echo '<br>';
+$object_to_delete5 = array();
+$object_to_delete5['id'] = 1062;
+echo $DB->delete_records('talentospilos_user_extended',$object_to_delete5);
+echo '<br>';
