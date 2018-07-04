9cbdf8b308fbd169286b17cd0ad08f1f86f528a4
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2018-07-04 15:48:26 -0500

    Especificacion y mejora de visualizacion de estimulo en el reporte

--- amd/src/historic_academic_reports.js
+++ amd/src/historic_academic_reports.js
@@ -17,13 +17,27 @@ define(['jquery', 'block_ases/bootstrap', 'block_ases/jquery.dataTables', 'block
             $(document).ready(function () {
 
                 ////Controles para la tabla historica academica
-			$(document).on('change', '#tableResultStudent thead tr th select', function () {
-				var table = $("#tableResultStudent").DataTable();
-		
-				var colIndex = $(this).parent().index()+1;
-				var selectedText=$(this).parent().find(":selected").text();
-				table.columns( colIndex-1 ).search( this.value ).draw();		
-			});
+                $(document).on('change', '#tableResultStudent thead tr th select', function () {
+                    var table = $("#tableResultStudent").DataTable();
+
+                    var colIndex = $(this).parent().index() + 1;
+                    var selectedText = $(this).parent().find(":selected").text();
+                    table.columns(colIndex - 1).search(this.value).draw();
+                });
+
+                $(document).on('click', '#tableResultStudent tbody tr td', function () {
+                    var table = $("#tableResultStudent").DataTable();
+                    var colIndex = table.cell(this).index().column;
+
+                    if (colIndex == 10) {
+                        if (table.cell(table.row(this).index(), 10).data() == 'SI') {
+                            var codigo = table.cell(table.row(this).index(), 2).data()
+                            var programa = table.cell(table.row(this).index(), 6).data()
+                            var semestre = table.cell(table.row(this).index(), 5).data()
+                            checkEstimulo(codigo, programa, semestre);
+                        }
+                    }
+                });
             });
         },
         load_table_students: function (data) {
@@ -44,4 +58,35 @@ define(['jquery', 'block_ases/bootstrap', 'block_ases/jquery.dataTables', 'block
             $("#tableResultTotal").DataTable(data);
         }
     };
+
+    function checkEstimulo(codigo, programa, semestre) {
+        $.ajax({
+            type: "POST",
+            data: {
+                codigo: codigo,
+                programa: programa,
+                type: "check_estimulo",
+                semestre: semestre,
+            },
+            url: "../managers/historic_academic_reports/historic_academic_reports_processing.php",
+            success: function (msg) {
+                console.log(msg);
+                swal({
+                    title: "Estímulo Académico",
+                    type: "info",
+                    text: msg,
+                    html: true,
+                    showCancelButton: false,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Cerrar",
+                    closeOnConfirm: true
+                });
+            },
+            dataType: "text",
+            cache: "false",
+            error: function (msg) {
+                console.log(msg);
+            },
+        });
+    }
 });
