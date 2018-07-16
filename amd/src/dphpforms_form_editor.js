c076e852c24408ff280988dae5416349935aaffd
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-07-16 11:42:54 -0500

    . min

--- amd/src/dphpforms_form_editor.js
+++ amd/src/dphpforms_form_editor.js
@@ -6,7 +6,7 @@
  */
  
  /**
-  * @module block_ases/dphpforms_form_builder
+  * @module block_ases/dphpforms_form_editor
   */
 
   define(['jquery', 'block_ases/bootstrap', 'block_ases/sweetalert', 'block_ases/jqueryui','block_ases/select2'], function($, bootstrap, sweetalert, jqueryui, select2) {
@@ -450,4 +450,4 @@
             }
     };
       
-})
+});
