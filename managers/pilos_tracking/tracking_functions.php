baa05448906fec1c8c2ccd7260d374de1dd39da4
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-07-18 10:21:14 -0500

    + FIX lista de seguimientos en lugares diferentes a la ficha.

--- managers/pilos_tracking/tracking_functions.php
+++ managers/pilos_tracking/tracking_functions.php
@@ -483,14 +483,15 @@ function trackings_sorting($array_detail_peer_trackings_dphpforms, $array_tracki
     for ($x = 0; $x < count($array_tracking_date); $x++) {
         $string_date = $array_tracking_date[$x];
         $array_tracking_date[$x] = getdate($array_tracking_date[$x]);
-        if (property_exists($seguimientos_ordenados, $array_tracking_date[$x]['year'])) {
+        $year = $array_tracking_date[$x]['year'];
+        if (property_exists($seguimientos_ordenados, $year)) {
             if (in_array($array_tracking_date[$x]['mon'], $periodo_a)) {
                 for ($y = 0; $y < count($array_detail_peer_trackings_dphpforms); $y++) {
                     if ($array_detail_peer_trackings_dphpforms[$y]) {
                         foreach($array_detail_peer_trackings_dphpforms[$y]->record->campos as & $tracking) {
                             if ($tracking->local_alias == 'fecha') {
                                 if (strtotime($tracking->respuesta) == $string_date) {
-                                    array_push($seguimientos_ordenados->$array_tracking_date[$x]['year']->per_a, $array_detail_peer_trackings_dphpforms[$y]);
+                                    array_push($seguimientos_ordenados->$year->per_a, $array_detail_peer_trackings_dphpforms[$y]);
                                     $array_detail_peer_trackings_dphpforms[$y] = null;
                                     break;
                                 }
@@ -505,7 +506,7 @@ function trackings_sorting($array_detail_peer_trackings_dphpforms, $array_tracki
                         foreach($array_detail_peer_trackings_dphpforms[$y]->record->campos as & $tracking) {
                             if ($tracking->local_alias == 'fecha') {
                                 if (strtotime($tracking->respuesta) == $string_date) {
-                                    array_push($seguimientos_ordenados->$array_tracking_date[$x]['year']->per_b, $array_detail_peer_trackings_dphpforms[$y]);
+                                    array_push($seguimientos_ordenados->$year->per_b, $array_detail_peer_trackings_dphpforms[$y]);
                                     $array_detail_peer_trackings_dphpforms[$y] = null;
                                     break;
                                 }
@@ -516,18 +517,18 @@ function trackings_sorting($array_detail_peer_trackings_dphpforms, $array_tracki
             }
         }
         else {
-            array_push($seguimientos_ordenados->index, $array_tracking_date[$x]['year']);
-            $seguimientos_ordenados->$array_tracking_date[$x]['year']->year = $array_tracking_date[$x]['year'];
-            $seguimientos_ordenados->$array_tracking_date[$x]['year']->per_a = array();
-            $seguimientos_ordenados->$array_tracking_date[$x]['year']->per_b = array();
-            $seguimientos_ordenados->$array_tracking_date[$x]['year']->year = $array_tracking_date[$x]['year'];
+            array_push($seguimientos_ordenados->index, $year);
+            $seguimientos_ordenados->$year->year = $year;
+            $seguimientos_ordenados->$year->per_a = array();
+            $seguimientos_ordenados->$year->per_b = array();
+            $seguimientos_ordenados->$year->year = $year;
             if (in_array($array_tracking_date[$x]['mon'], $periodo_a)) {
                 for ($y = 0; $y < count($array_detail_peer_trackings_dphpforms); $y++) {
                     if ($array_detail_peer_trackings_dphpforms[$y]) {
                         foreach($array_detail_peer_trackings_dphpforms[$y]->record->campos as & $tracking) {
                             if ($tracking->local_alias == 'fecha') {
                                 if (strtotime($tracking->respuesta) == $string_date) {
-                                    array_push($seguimientos_ordenados->$array_tracking_date[$x]['year']->per_a, $array_detail_peer_trackings_dphpforms[$y]);
+                                    array_push($seguimientos_ordenados->$year->per_a, $array_detail_peer_trackings_dphpforms[$y]);
                                     $array_detail_peer_trackings_dphpforms[$y] = null;
                                     break;
                                 }
@@ -542,7 +543,7 @@ function trackings_sorting($array_detail_peer_trackings_dphpforms, $array_tracki
                         foreach($array_detail_peer_trackings_dphpforms[$y]->record->campos as & $tracking) {
                             if ($tracking->local_alias == 'fecha') {
                                 if (strtotime($tracking->respuesta) == $string_date) {
-                                    array_push($seguimientos_ordenados->$array_tracking_date[$x]['year']->per_b, $array_detail_peer_trackings_dphpforms[$y]);
+                                    array_push($seguimientos_ordenados->$year->per_b, $array_detail_peer_trackings_dphpforms[$y]);
                                     $array_detail_peer_trackings_dphpforms[$y] = null;
                                     break;
                                 }
