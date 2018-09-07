2682053091f0dd27a96ebee4b1083a5e14816ec2
Iader E. García Gómez <iader.garcia@correounivalle.edu.co>
2018-09-07 11:26:11 -0500

    Reporte formularios

---
+++ managers/dphpforms/dphpforms_dwarehouse_api.php
@@ -0,0 +1,169 @@
+<?php 
+    // This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Dynamic PHP Forms
+ *
+ * @author     Juan Pablo Castro
+ * @package    block_ases
+ * @copyright  2018 Juan Pablo Castro<juan.castro.vasquez@correounivalle.edu.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Standard GPL and phpdocs
+
+    require_once(dirname(__FILE__). '/../../../../config.php');
+    require_once(dirname(__FILE__).'/dphpforms_dwarehouse_lib.php');
+
+    header('Content-Type: application/json');
+
+    global $USER;
+    
+    $raw_data = file_get_contents("php://input");
+    
+    // Validation if the user is logged. 
+    // if( $USER->id == 0 ){
+    //    return_with_code( -1 );
+    //}
+
+    $input = json_decode( $raw_data );
+
+    /**
+     * Api la consulta de datos respecto a usuarios, y formularios.
+     * @author Juan Pablo Castro
+     * @see user_management_lib.php
+     * @param json $input This input is a json with a function name and their respective parameters. The order of these parameters is very important. See every function to notice of their parameters order.
+     * @return json The structure is {"status_code":int, "error_message":string, "data_response":string }
+    */
+
+    // Example of valid input. params = Parameters
+    // { "function":"LA_FUNCION", "params":[ id_user , id_form ] }
+
+
+
+    if( isset($input->function) && isset($input->params) ){
+
+        if( $input->function == "get_user_form" ){
+
+            /* In this request is only valid pass like param(Parameters) the instance identificatior, 
+             * for this reason, the input param only can be equal in quantity to one.
+             * */
+            
+            if( count( $input->params ) == 1 ){
+
+                
+                // Order of params
+                /*
+                 * The id_form value only can be a number.
+                 */
+                
+                if( is_numeric( $input->params[0] )  ){
+                    
+                    echo json_encode( 
+                        array(
+                            "status_code" => 0,
+                            "error_message" => "",
+                            "data_response" => dphpforms_get_only_form_dwarehouse( $input->params[0] )
+                        )
+                    );
+                    
+                }else{
+                    return_with_code( -2 );
+                }
+            }else{
+                return_with_code( -2 );
+            }
+
+        }else{
+            // Function not defined
+            return_with_code( -4 );
+        }
+        
+    }else{
+        return_with_code( -2 );
+    }
+
+
+
+
+
+    function return_with_code( $code ){
+        
+        switch( $code ){
+
+            case -1:
+                echo json_encode(
+                    array(
+                        "status_code" => $code,
+                        "error_message" => "You are not allowed to access this resource.",
+                        "data_response" => ""
+                    )
+                );
+                break;
+            case -2:
+                echo json_encode(
+                    array(
+                        "status_code" => $code,
+                        "error_message" => "Error in the scheme.",
+                        "data_response" => ""
+                    )
+                );
+                break;
+            case -3:
+                echo json_encode(
+                    array(
+                        "status_code" => $code,
+                        "error_message" => "Invalid values in the parameters.",
+                        "data_response" => ""
+                    )
+                );
+                break;
+            case -4:
+                echo json_encode(
+                    array(
+                        "status_code" => $code,
+                        "error_message" => "Function not defined.",
+                        "data_response" => ""
+                    )
+                );
+                break;
+            
+            case -5:
+                echo json_encode(
+                    array(
+                        "status_code" => $code,
+                        "error_message" => "Duplicate.",
+                        "data_response" => ""
+                    )
+                );
+                break;
+            
+            case -99:
+                echo json_encode(
+                    array(
+                        "status_code" => $code,
+                        "error_message" => "critical error.",
+                        "data_response" => ""
+                    )
+                );
+                break;
+
+        }
+
+        die();
+    }
+
+?>
--- managers/dphpforms/dphpforms_dwarehouse_lib.php
+++ managers/dphpforms/dphpforms_dwarehouse_lib.php
@@ -26,23 +26,95 @@
 require_once(dirname(__FILE__). '/../../../../config.php');
 require_once $CFG->dirroot.'/blocks/ases/managers/lib/lib.php';
 
-function dphpforms_dwarehouse(){
+function dphpforms_get_forms_dwarehouse(){
     global $DB;
-    $msg = "UPDATE";
-    $sql = "SELECT *   FROM {talentospilos_df_dwarehouse} AS df_dwarehouse WHERE df_dwarehouse.accion = $msg";
+    $forms_dwarehouse_array = array();
+ 
+    $sql = "SELECT id_usuario_moodle AS id_user, firstname AS name_user, accion AS name_accion, fecha_hora_registro AS fecha_act  
+            FROM {talentospilos_df_dwarehouse} AS dwarehouse INNER JOIN {user}  ON mdl_user.id = dwarehouse.id_usuario_moodle WHERE dwarehouse.id_usuario_moodle = mdl_user.id ";
+    $results = $DB->get_records_sql($sql);
+
+    foreach ($results as $record) {
+     
+
+        array_push($forms_dwarehouse_array, $record);
+    }
+
     
-   
+   return $forms_dwarehouse_array;
 }
 
-function get_array_for_reports($instance_id){
+function dphpforms_get_only_form_dwarehouse($id_form){
+    global $DB;
+    $form_dwarehouse_array = array();
+    $id_form= 1; 
+
+    $sql = "SELECT id_usuario_moodle AS id_user, accion AS name_action, datos_previos AS dp_form, datos_enviados AS de_form, datos_almacenados AS da_form, 
+    fecha_hora_registro AS fecha_form FROM {talentospilos_df_dwarehouse} AS dwarehouse WHERE dwarehouse.id = $id_form ";
+
+    $results = $DB->get_records_sql($sql);
+    foreach ($results as $record) {
+        array_push($form_dwarehouse_array, $record);
+    }
+    return $form_dwarehouse_array;
+}
+
+function get_only_report($instance_id){
     $columns = array();
-    array_push($columns, array("title"=>"Id usuario", "name"=>"id_usuario", "data"=>"1"));
-    array_push($columns, array("title"=>"Acción", "name"=>"fullname", "data"=>"Hola"));
-    array_push($columns, array("title"=>"Fecha y hora", "name"=>"fecha_hora", "data"=>"Hoy"));
+    array_push($columns, array("title"=>"Id usuario", "name"=>"id_user", "data"=>"id_user"));
+    
+    array_push($columns, array("title"=>"Acción", "name"=>"name_action", "data"=>"name_action"));
+    array_push($columns, array("title"=>"Datos previos", "name"=>"dp_form", "data"=>"dp_form"));
+    array_push($columns, array("title"=>"Datos enviados", "name"=>"de_form", "data"=>"de_form"));
+    array_push($columns, array("title"=>"Datos almacenados", "name"=>"da_form", "data"=>"da_form"));
+    array_push($columns, array("title"=>"Fecha", "name"=>"fecha_form", "data"=>"fecha_form"));
 
     $data = array(
         "bsort" => false,
         "columns" => $columns,
+        "data" => dphpforms_get_only_form_dwarehouse(),
+        "language" => 
+         array(
+            "search"=> "Buscar:",
+            "oPaginate" => array(
+                "sFirst"=>    "Primero",
+                "sLast"=>     "Último",
+                "sNext"=>     "Siguiente",
+                "sPrevious"=> "Anterior"
+                ),
+            "sProcessing"=>     "Procesando...",
+            "sLengthMenu"=>     "Mostrar _MENU_ registros",
+            "sZeroRecords"=>    "No se encontraron resultados",
+            "sEmptyTable"=>     "Ningún dato disponible en esta tabla",
+            "sInfo"=>           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty"=>      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered"=>   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix"=>    "",
+            "sSearch"=>         "Buscar:",
+            "sUrl"=>            "",
+            "sInfoThousands"=>  ",",
+            "sLoadingRecords"=> "Cargando...",
+         ),
+        "order"=> array(0, "desc")
+
+    );
+
+    return $data;
+
+}
+
+
+function get_array_for_reports($instance_id){
+    $columns = array();
+    array_push($columns, array("title"=>"Id usuario", "name"=>"id_user", "data"=>"id_user"));
+    array_push($columns, array("title"=>"Nombre", "name"=>"name_user", "data"=>"name_user"));
+    array_push($columns, array("title"=>"Acción", "name"=>"name_accion", "data"=>"name_accion"));
+    array_push($columns, array("title"=>"Fecha", "name"=>"fecha_act", "data"=>"fecha_act"));
+
+    $data = array(
+        "bsort" => false,
+        "columns" => $columns,
+        "data" => dphpforms_get_forms_dwarehouse(),
         "language" => 
          array(
             "search"=> "Buscar:",
