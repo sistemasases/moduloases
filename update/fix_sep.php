c3560ebe5ede84c7f111df548197289790b786ff
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-09-18 19:00:13 -0500

    +.

--- update/fix_sep.php
+++ update/fix_sep.php
@@ -10,3 +10,4 @@ require_once(dirname(__FILE__). '/../../../config.php');
 global $DB;
 
 print_r($DB->delete_records($table, array('id'=>'7820')));
+print_r($DB->delete_records($table, array('id'=>'7742')));
