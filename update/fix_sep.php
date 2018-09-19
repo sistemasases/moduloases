a510cbfe26b7b54557ad3bdae731767f10180c63
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-09-18 19:09:43 -0500

    .

--- update/fix_sep.php
+++ update/fix_sep.php
@@ -2,11 +2,6 @@
 
 require_once(dirname(__FILE__). '/../../../config.php');
 
-/* Modifica registros en la tabla {talentospilos_user_extended}
-   cuando :
-   El id_ases_user este repetido y en ambos tracking_status este en 1
-*/
-
 global $DB;
 $table = 'talentospilos_usuario';
 print_r($DB->delete_records($table, array('id'=>'7820')));
