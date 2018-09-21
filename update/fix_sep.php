fcc6d69bb2df3253f5ff5a4a32f8f7cc14bed62c
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-09-21 13:28:55 -0500

    Control

--- update/fix_sep.php
+++ update/fix_sep.php
@@ -11,9 +11,27 @@ print_r($DB->delete_records($table, array('id'=>'7742')));*/
 //UPDATE mdl_talentospilos_user_extended SET id_ases_user = 7446, tracking_status = 0 WHERE id = 531
 
 $record = $DB->get_record_sql("SELECT * FROM {talentospilos_user_extended} WHERE id = 531");
+print_r( json_encode($record) );
+
 $record->tracking_status = 0;
 $record->id_ases_user = 7446;
-print_r( $DB->update_record( 'talentospilos_usuario', $record ) );
-print_r( $DB->delete_records( 'talentospilos_usuario', array( 'id'=>'837' ) ) );
+
+try {
+    echo "Actualizando...<br>";
+    print_r( $DB->update_record( 'talentospilos_usuario', $record ) );
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "<br><br>";
+}
+
+try {
+    echo "Eliminando...<br>";
+    print_r( $DB->delete_records( 'talentospilos_usuario', array( 'id'=>'837' ) ) );
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "<br><br>";
+}
+
+echo "Resultado:<br>";
+$record = $DB->get_record_sql("SELECT * FROM {talentospilos_user_extended} WHERE id = 531");
+print_r( json_encode($record) );
 
 die();
