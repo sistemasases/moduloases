45c5f7ec93e3489aecac40b2016c105826fdce2e
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-09-21 16:47:59 -0500

    .

--- update/fix_sep.php
+++ update/fix_sep.php
@@ -18,7 +18,7 @@ $record->id_ases_user = 7446;
 
 try {
     echo "<br><br>Actualizando...<br>";
-    print_r( $DB->update_record( 'talentospilos_usuario', $record ) );
+    print_r( $DB->update_record( 'talentospilos_user_extended', $record ) . "<br>" );
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "<br><br>";
 }
@@ -32,6 +32,28 @@ try {
 
 echo "Resultado:<br>";
 $record = $DB->get_record_sql("SELECT * FROM {talentospilos_user_extended} WHERE id = 531");
+print_r( json_encode($record) . "<br><br>" );
+
+// --------------
+
+echo "Datos previos...<br>";
+$record = $DB->get_record_sql("SELECT * FROM {talentospilos_user_extended} WHERE id_moodle_user = 115914");
 print_r( json_encode($record) );
+
+$record->tracking_status = 0;
+$record->id_moodle_user = 93335;
+$record->id_academic_program = 60;
+
+try {
+    echo "<br><br>Actualizando...<br>";
+    print_r( $DB->update_record( 'talentospilos_user_extended', $record ) . "<br>" );
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "<br><br>";
+}
+
+echo "Resultado:<br>";
+$records = $DB->get_records_sql("SELECT * FROM {talentospilos_user_extended} WHERE id_moodle_user = 93335");
+print_r( json_encode($records) );
+
 
 die();
