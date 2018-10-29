8a2a81ce9378a948a3253c7c4188bfd45243a130
juanpamm <moreno.juan@correounivalle.edu.co>
2018-10-29 11:59:11 -0500

    Script para actualizar registros

---
+++ update/update_icetex_users_records.php
@@ -0,0 +1,147 @@
+<?php
+
+require_once(dirname(__FILE__). '/../../../config.php');
+
+global $DB;
+/** Modificaciones relacionadas al estudiante: JIMENEZ LEGARDA MANUELA */
+
+//Actualización registro en ICETEX
+$icetex_record_1 = new StdClass;
+$icetex_record_1->id = 2239;
+$icetex_record_1->id_estudiante = 391;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_1);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_1 = new StdClass;
+$usuario_record_1->id = 6741;
+$usuario_record_1->num_doc = '9999999999';
+$usuario_record_1->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_1);
+
+
+/** Modificaciones relacionadas al estudiante: LOPEZ MORALES KARINA */
+
+//Actualización registro en ICETEX
+$icetex_record_2 = new StdClass;
+$icetex_record_2->id = 2240;
+$icetex_record_2->id_estudiante = 95;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_2);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_2 = new StdClass;
+$usuario_record_2->id = 897;
+$usuario_record_2->num_doc = '9999999999';
+$usuario_record_2->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_2);
+
+
+/** Modificaciones relacionadas al estudiante: RIASCOS ZAMORA CRISTIAN ALEJANDRO */
+
+//Actualización registro en ICETEX
+$icetex_record_3 = new StdClass;
+$icetex_record_3->id = 2254;
+$icetex_record_3->id_estudiante = 138;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_3);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_3 = new StdClass;
+$usuario_record_3->id = 6947;
+$usuario_record_3->num_doc = '9999999999';
+$usuario_record_3->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_3);
+
+
+/** Modificaciones relacionadas al estudiante: LEON ADARVE JESSICA JOANA */
+
+//Actualización registro en ICETEX
+$icetex_record_4 = new StdClass;
+$icetex_record_4->id = 2002;
+$icetex_record_4->id_estudiante = 91;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_4);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_4 = new StdClass;
+$usuario_record_4->id = 896;
+$usuario_record_4->num_doc = '9999999999';
+$usuario_record_4->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_4);
+
+
+/** Modificaciones relacionadas al estudiante: ORTEGA CAJIBIOY ALBA JUDITH */
+
+//Actualización registro en ICETEX
+$icetex_record_5 = new StdClass;
+$icetex_record_5->id = 2008;
+$icetex_record_5->id_estudiante = 119;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_5);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_5 = new StdClass;
+$usuario_record_5->id = 899;
+$usuario_record_5->num_doc = '9999999999';
+$usuario_record_5->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_5);
+
+
+/** Modificaciones relacionadas al estudiante: ORTEGA CAJIBIOY ALBA JUDITH */
+
+//Actualización registro en ICETEX
+$icetex_record_6 = new StdClass;
+$icetex_record_6->id = 1870;
+$icetex_record_6->id_estudiante = 7107;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_6);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_6 = new StdClass;
+$usuario_record_6->id = 7863;
+$usuario_record_6->num_doc = '9999999999';
+$usuario_record_6->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_6);
+
+
+/** Modificaciones relacionadas al estudiante: LOPEZ GARCIA VALENTINA */
+
+//Actualización registro en ICETEX
+$icetex_record_7 = new StdClass;
+$icetex_record_7->id = 1879;
+$icetex_record_7->id_estudiante = 488;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_7);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_7 = new StdClass;
+$usuario_record_7->id = 7119;
+$usuario_record_7->num_doc = '9999999999';
+$usuario_record_7->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_7);
+
+ 
+/** Modificaciones relacionadas al estudiante: ORDOÑEZ GOMEZ JOSE FERNANDO */
+
+//Actualización registro en ICETEX
+$icetex_record_8 = new StdClass;
+$icetex_record_8->id = 1888;
+$icetex_record_8->id_estudiante = 528;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_8);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_8 = new StdClass;
+$usuario_record_8->id = 7019;
+$usuario_record_8->num_doc = '9999999999';
+$usuario_record_8->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_8);
+
+
+/** Modificaciones relacionadas al estudiante: SIMALES ORDOÑEZ CINDY NATALIA */
+
+//Actualización registro en ICETEX
+$icetex_record_9 = new StdClass;
+$icetex_record_9->id = 1897;
+$icetex_record_9->id_estudiante = 7113;
+$DB->update_record('talentospilos_res_estudiante', $icetex_record_9);
+
+//Modificación de registro fantasma en talentospilos_usuario
+$usuario_record_9 = new StdClass;
+$usuario_record_9->id = 7865;
+$usuario_record_9->num_doc = '9999999999';
+$usuario_record_9->num_doc_ini = '9999999999';
+$DB->update_record('talentospilos_usuario', $usuario_record_9);
