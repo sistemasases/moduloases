e0ecea3b7996300aa247a36709fd81b7db9184a3
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-11-06 14:21:10 -0500

    Se asocia hoja de estilos.

--- view/report_trackings.php
+++ view/report_trackings.php
@@ -192,6 +192,7 @@ $PAGE->requires->css('/blocks/ases/style/c3.css', true);
 $PAGE->requires->css('/blocks/ases/style/student_profile_risk_graph.css', true);
 $PAGE->requires->css('/blocks/ases/js/select2/css/select2.css', true);
 $PAGE->requires->css('/blocks/ases/style/side_menu_style.css', true);
+$PAGE->requires->css('/blocks/ases/style/report_trackings.css', true);
 //Pendiente para cambiar el idioma del nombre del archivo junto con la estructura de
 //su nombramiento.
 $PAGE->requires->css('/blocks/ases/style/creadorFormulario.css', true);
