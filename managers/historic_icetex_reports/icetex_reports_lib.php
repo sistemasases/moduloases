f19d500b18163a4fff554de80843567fe4980703
juanpamm <moreno.juan@correounivalle.edu.co>
2018-11-10 14:48:49 -0500

    Actualización del reporte de estudiantes y resoluciones.
    Se añade columna que representa el estado del estudiante frente a SRA e ICETEX.

--- managers/historic_icetex_reports/icetex_reports_lib.php
+++ managers/historic_icetex_reports/icetex_reports_lib.php
@@ -51,11 +51,12 @@ function get_array_students_with_resolution(){
 						WHEN (res_students.codigo_resolucion IS NOT NULL)
 								THEN res_students.codigo_resolucion
                     END AS codigo_resolucion,
-                    CASE WHEN (spp_students.program_status = '-ACTIVO' AND status_icetex.nombre_estado LIKE '5%')
-                                THEN 'ACTIVO'
-                        WHEN (spp_students.program_status = '-INACTIVO' OR spp_students.program_status = '---'
-                                OR status_icetex.nombre_estado LIKE '3%' OR status_icetex.nombre_estado IS NULL)
-                                THEN 'INACTIVO'
+                    CASE WHEN ( (academic_students.fecha_cancel IS NULL AND academic_students.promedio_semestre IS NOT NULL) 
+                                    AND status_icetex.nombre_estado LIKE '5%')
+                                THEN '-ACTIVO'
+                        WHEN (academic_students.promedio_semestre IS NULL OR status_icetex.nombre_estado LIKE '3%' 
+                                    OR status_icetex.nombre_estado IS NULL)
+                                THEN '-INACTIVO'
                     END AS est_ice_sra
                 FROM
                 (SELECT user_extended.id_ases_user, moodle_user.lastname, moodle_user.firstname, cohorts.idnumber, semestre.id AS id_semestre, semestre.nombre AS nombre_semestre, 
