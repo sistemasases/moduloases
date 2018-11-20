01fdf6873a31c61dda64b5a8781b7957cad365a8
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-11-20 16:21:19 -0500

    Se cambian los indicadores de ficha e inasistencia por íconos.

--- view/report_trackings.php
+++ view/report_trackings.php
@@ -182,6 +182,7 @@ $data->table_periods =$table_periods;
 $data->table=$table_permissions;
 $data->semester = $semester_id;
 
+
 $PAGE->requires->css('/blocks/ases/style/base_ases.css', true);
 $PAGE->requires->css('/blocks/ases/style/jqueryui.css', true);
 $PAGE->requires->css('/blocks/ases/style/styles_pilos.css', true);
@@ -195,6 +196,7 @@ $PAGE->requires->css('/blocks/ases/style/student_profile_risk_graph.css', true);
 $PAGE->requires->css('/blocks/ases/js/select2/css/select2.css', true);
 $PAGE->requires->css('/blocks/ases/style/side_menu_style.css', true);
 $PAGE->requires->css('/blocks/ases/style/report_trackings.css', true);
+$PAGE->requires->css('/blocks/ases/style/fontawesome550.min.css', true);
 //Pendiente para cambiar el idioma del nombre del archivo junto con la estructura de
 //su nombramiento.
 $PAGE->requires->css('/blocks/ases/style/creadorFormulario.css', true);
