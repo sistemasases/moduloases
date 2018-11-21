c072790c0b79e6f221f5e550f98474a2f7fc7237
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-11-21 11:01:34 -0500

    fix(course_and_teacher_report): deleted dupulicated function for select ases courses (the function is in course lib

--- managers/course_and_teacher_report/course_and_teacher_report_lib.php
+++ managers/course_and_teacher_report/course_and_teacher_report_lib.php
@@ -2,7 +2,7 @@
 
 
 use function jquery_datatable\get_datatable_class_column;
-
+require_once(__DIR__ . '/../../../../config.php');
 require_once (__DIR__ . '/../jquery_datatable/jquery_datatable_lib.php');
 require_once (__DIR__ . '/../../managers/periods_management/periods_lib.php');
 require_once (__DIR__ . '/../../classes/DAO/BaseDAO.php');
@@ -11,55 +11,14 @@ require_once (__DIR__ . '/../course/course_lib.php');
 
 require_once(__DIR__.'/../../vendor/autoload.php');
 
-use Latitude\QueryBuilder\Query\SelectQuery;
-use function Latitude\QueryBuilder\{ alias, on, field, QueryInterface, criteria, literal };
-
-require_once (__DIR__ . '/../course/course_lib.php');
-
-require_once(__DIR__.'/../../vendor/autoload.php');
+error_reporting(E_ALL | E_STRICT);   // NOT FOR PRODUCTION SERVERS!
+ini_set('display_errors', '1');         // NOT FOR PRODUCTION SERVERS!
+$CFG->debug = (E_ALL | E_STRICT);   // === DEBUG_DEVELOPER - NOT FOR PRODUCTION SERVERS!
+$CFG->debugdisplay = 1;
 
 use Latitude\QueryBuilder\Query\SelectQuery;
 use function Latitude\QueryBuilder\{ alias, on, field, QueryInterface, criteria, literal };
-
-
-function _select_ases_courses($semestre, $id_instancia): SelectQuery {
-
-
-    return BaseDAO::get_factory()
-        ->select(
-            literal('DISTINCT ON (mdl_course.id) *')
-        )
-        ->from(alias('{talentospilos_user_extended}', 'user_extended'))
-        ->innerJoin(
-            alias('{user}', 'mdl_user'),
-            on('mdl_user.id', 'user_extended.id_moodle_user'))
-        ->innerJoin(
-            alias('{cohort_members}', 'mdl_cohort_members'),
-            on('mdl_cohort_members.userid', 'mdl_user.id'))
-        ->innerJoin(
-            alias('{talentospilos_inst_cohorte}', 'inst_cohorte'),
-            on('inst_cohorte.id_cohorte', 'mdl_cohort_members.cohortid'))
-        ->innerJoin(
-            alias('{role_assignments}', 'mdl_role_assignments'),
-            on('mdl_role_assignments.userid', 'mdl_user.id'))
-        ->innerJoin(
-            alias('{context}', 'mdl_context'),
-            on('mdl_role_assignments.contextid', 'mdl_context.id'))
-        ->innerJoin(
-            alias('{course}', 'mdl_course'),
-            on('mdl_course.id', 'mdl_context.instanceid'))
-        ->innerJoin(
-            alias('{talentospilos_est_estadoases}', 'est_estadoases'),
-            on('user_extended.id_ases_user', 'est_estadoases.id_estudiante'))
-        ->innerJoin(
-            alias('{talentospilos_estados_ases}', 'estados_ases'),
-            on('est_estadoases.id_estado_ases', 'estados_ases.id'))
-        ->where(field('estados_ases.nombre')->eq('seguimiento'))
-        ->andWhere(field('inst_cohorte.id_instancia')->eq($id_instancia))
-        ->andWhere(field('user_extended.tracking_status')->eq(1))
-        ->andWhere(field('mdl_role_assignments.roleid')->eq(5))
-        ->andWhere(criteria("substring(mdl_course.shortname from 15 for 6) = %s", $semestre));
-}
+error_reporting(E_ALL);
 
 /**
  * Class ItemReporteCursoProfesores
@@ -349,8 +308,6 @@ INNER JOIN
         and substring(mdl_course.shortname from 15 for 6) = '$semestre' ) AS moodle_course
         ON              moodle_course.curso_id = mdl_context.instanceid
         WHERE           mdl_role_assignments.roleid = 3
-
-
 SQL;
 
     return $DB->get_records_sql($sql);
