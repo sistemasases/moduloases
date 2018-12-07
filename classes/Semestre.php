85026a4d204d38cc49b6184aacedcb02e8b3ef38
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-12-07 17:05:34 -0500

    fix(dao): remove print date when this is validating its format

--- classes/Semestre.php
+++ classes/Semestre.php
@@ -93,7 +93,6 @@ SQL;
     static function  validateDate($date, $format = 'Y-m-d')
     {
         $d = DateTime::createFromFormat($format, $date);
-        print_r($d);
         // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
         return $d && $d->format($format) === $date;
     }
