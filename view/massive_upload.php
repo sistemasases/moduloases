b089817df3913fe00adfe6289cbb6c98a0e7dd4c
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-02-21 10:50:38 -0500

    chore(mass_management): add file headers with autor

--- view/massive_upload.php
+++ view/massive_upload.php
@@ -1,5 +1,30 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Common view for the massive data upload process
+ *
+ * @author     Luis Gerardo Manrique Cardona
+ * @package    block_ases
+ * @copyright  2016 Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+
 require_once (__DIR__ . '/../../../config.php');
 require_once (__DIR__ . '/../classes/output/renderer.php');
 require_once (__DIR__ . '/../classes/output/massive_upload_component.php');
