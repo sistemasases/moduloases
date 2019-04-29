382340f9b0505ec74ee7df97b78e7780af2ad82e
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2019-04-29 15:25:39 -0500

    Secutiry core base

---
+++ core/security/v1/entrypoint.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * @package		block_ases
+ * @subpackage	core.security
+ * @author 		Jeison Cardona Gómez
+ * @copyright 	(C) 2019 Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
+ * @license   	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once( __DIR__ . "/../../../../../config.php");
+
+
+
+
+
+?>
