18be07c2c9de2c9515b5ba1922df6e8393ad132e
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2019-05-10 05:31:05 -0500

    Update secutiry.php

--- core/security/secutiry.php
+++ core/security/secutiry.php
@@ -7,7 +7,7 @@
  * @license   	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-const VERSION = 1; //Current version
+const VERSION = 1; //Current version.
 
 require_once( __DIR__ . "/../../../../config.php");
 require_once( __DIR__ . "/../module_loader.php");
