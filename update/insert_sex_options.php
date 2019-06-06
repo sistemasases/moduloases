38eb4594bf8087e8a6ec2dd649ef88cdca4d3785
jpcv222 <juan.castro.vasquez@correounivalle.edu.co>
2019-06-06 17:39:53 -0500

    Se valida la existencia de un registro en la tabla 'talentospilos_sexo' previo a insercion.

--- update/insert_sex_options.php
+++ update/insert_sex_options.php
@@ -7,17 +7,26 @@ $new_sex = new stdClass();
 $new_sex->sexo  = 'Intersexual';
 $new_sex->opcion_general  = 1;
 
-if($DB->insert_record('talentospilos_sexo', $new_sex)){
-    echo "Éxito insertando opción".$new_sex->sexo;
+if(!$DB->record_exists("talentospilos_sexo",array('sexo'=> $new_sex->sexo ))){
+    if($DB->insert_record('talentospilos_sexo', $new_sex)){
+        echo "Éxito insertando opción ".$new_sex->sexo;
+    }
+}else{
+    echo "Ya existe la opción ".$new_sex->sexo;
 }
+
 
 
 $new_sex = new stdClass();
 $new_sex->sexo  = 'NO REGISTRA';
 $new_sex->opcion_general  = 1;
 
-if($DB->insert_record('talentospilos_sexo', $new_sex)){
-    echo "Éxito insertando opción".$new_sex->sexo;
+if(!$DB->record_exists("talentospilos_sexo", array('sexo'=> $new_sex->sexo ))){
+    if($DB->insert_record('talentospilos_sexo', $new_sex)){
+        echo "Éxito insertando opción ".$new_sex->sexo;
+    }
+}else{
+    echo "Ya existe la opción ".$new_sex->sexo;
 }
 
 
