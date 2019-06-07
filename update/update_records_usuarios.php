628c102bd49fc9356c6a80ea4b8668282f81787a
juanpamm <moreno.juan@correounivalle.edu.co>
2019-06-07 11:28:07 -0500

    Se agregan mensajes de exito y error

--- update/update_records_usuarios.php
+++ update/update_records_usuarios.php
@@ -15,32 +15,59 @@ $record_to_update->id = 10052;
 $record_to_update->num_doc = '9999999929';
 $record_to_update->num_doc_ini = '9999999929';
 
-$DB->update_record('talentospilos_usuario', $record_to_update);
+if($DB->update_record('talentospilos_usuario', $record_to_update)){
+    echo 'Registro actualizado con éxito';
+}else{
+    echo 'Error';
+}
 
 $record_to_update1 = new StdClass;
 $record_to_update1->id = 10053;
 $record_to_update1->num_doc = '9999999919';
 $record_to_update1->num_doc_ini = '9999999919';
 
-$DB->update_record('talentospilos_usuario', $record_to_update1);
+if($DB->update_record('talentospilos_usuario', $record_to_update1)){    
+    echo 'Registro actualizado con éxito';
+}else{
+    echo 'Error';
+}
 
 $record_to_update2 = new StdClass;
 $record_to_update2->id = 10054;
 $record_to_update2->num_doc = '9999999909';
 $record_to_update2->num_doc_ini = '9999999909';
 
-$DB->update_record('talentospilos_usuario', $record_to_update2);
+if($DB->update_record('talentospilos_usuario', $record_to_update2)){
+    echo 'Registro actualizado con éxito';
+}else{
+    echo 'Error';
+}
 
 //Registros a eliminar
 
 $object_to_delete = array();
 $object_to_delete['id'] = 7910;
-$DB->delete_records('talentospilos_user_extended', $object_to_delete);
+
+if($DB->delete_records('talentospilos_user_extended', $object_to_delete)){
+    echo 'Registro actualizado con éxito';
+}else{
+    echo 'Error';
+}
 
 $object_to_delete1 = array();
 $object_to_delete1['id'] = 7911;
-$DB->delete_records('talentospilos_user_extended', $object_to_delete1);
+
+if($DB->delete_records('talentospilos_user_extended', $object_to_delete1)){
+    echo 'Registro actualizado con éxito';
+}else{
+    echo 'Error';
+}
 
 $object_to_delete2 = array();
 $object_to_delete2['id'] = 7912;
-$DB->delete_records('talentospilos_user_extended', $object_to_delete2);
+
+if($DB->delete_records('talentospilos_user_extended', $object_to_delete2)){
+    echo 'Registro actualizado con éxito';
+}else{
+    echo 'Error';
+}
