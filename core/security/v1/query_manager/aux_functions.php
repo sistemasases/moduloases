626fc8672730f52fc2a921d226f06c09cc6be89f
vvbv <jeison.cardona@correounivalle.edu.co>
2019-06-14 18:05:02 -0500

    menor

--- core/security/v1/query_manager/aux_functions.php
+++ core/security/v1/query_manager/aux_functions.php
@@ -1,12 +1,12 @@
 <?php
+
 /**
- * @package		block_ases
+ * @package	block_ases
  * @subpackage	core.security
- * @author 		Jeison Cardona Gómez
+ * @author 	Jeison Cardona Gómez
  * @copyright 	(C) 2019 Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
  * @license   	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 
 /**
  * Function that given a SQL query, ...
@@ -17,32 +17,36 @@
  * @param $query
  * 
  * @return bool|null
-*/
-function _is_select( $query ){
-	if( $query ){
-		$words = explode(' ',trim($query));
-		if( strtolower(trim($words[0])) == "select" ){
-			return true;
-		}else{
-			return false;
-		}
-	}else{
-		return null;
-	}
-
+ */
+function _is_select($query) {
+    if ($query) {
+        $words = explode(' ', trim($query));
+        if (strtolower(trim($words[0])) == "select") {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return null;
+    }
 }
 
 /**
+ * Function that return a list of managers available 
  * @author Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
-*/
-function _get_list_of_available_managers(){	return AVAILABLE_MANAGERS; };
-
+ * @since 1.0.0
+ * 
+ * @return array List of managers
+ */
+function _get_list_of_available_managers() {
+    return AVAILABLE_MANAGERS;
+}
 
 function _strpos_all($haystack, $needle) {
     $offset = 0;
     $allpos = array();
     while (($pos = strpos($haystack, $needle, $offset)) !== FALSE) {
-        $offset   = $pos + 1;
+        $offset = $pos + 1;
         $allpos[] = $pos;
     }
     return $allpos;
