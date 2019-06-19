4195785c236c31aad5e3cca1fa77367cc425d65f
vvbv <jeison.cardona@correounivalle.edu.co>
2019-06-19 15:45:59 -0500

    menor

--- core/security/security.php
+++ core/security/security.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package		block_ases
+ * @package	block_ases
  * @subpackage	core.security
- * @author 		Jeison Cardona Gómez
+ * @author 	Jeison Cardona Gómez
  * @copyright 	(C) 2019 Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
  * @license   	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
