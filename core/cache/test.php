1c22dd956dbd1b299ef4ffa1bcf5705800ea3231
vvbv <jeison.cardona@correounivalle.edu.co>
2019-07-15 17:53:48 -0500

    core cache tests

---
+++ core/cache/test.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once( __DIR__ . "/../module_loader.php" );
+
+module_loader( "cache" );
+
+//print_r( cache_key_exist( "v15" ) );
+//print_r( put_value( "v15", "LLA", "ASDD" ) );
+//print_r(get_cache_obj( "v15555" ) );
+//print_r(get_value( "v15" ) );
+
+//print_r(delete_from_cache("v15") );
+
+//print_r( update_value( "v14", "P1" ) );
+
+//print_r(update_description( "v14", "desc P1" ) );
+
+?>
