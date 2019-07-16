d9bb97ed7f546c25d48429b12549247967d2acae
vvbv <jeison.cardona@correounivalle.edu.co>
2019-07-16 14:58:44 -0500

    minor

--- core/cache/test.php
+++ core/cache/test.php
@@ -8,4 +8,8 @@ module_loader( "cache" );
 //print_r(core_cache_get_value(-1500) );
 //core_cache_delete(-1500);
 
+//print_r(core_cache_get_value("v2") );
+//print_r(core_cache_update_value("v2" , "Test2") );
+//print_r(core_cache_get_value("v2") );
+
 ?>
