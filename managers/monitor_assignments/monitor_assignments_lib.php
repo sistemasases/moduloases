a80c5db313648e264d46c6310613b181f02dc3f8
vvbv <jeison.cardona@correounivalle.edu.co>
2019-09-25 18:14:46 -0500

    minor

--- managers/monitor_assignments/monitor_assignments_lib.php
+++ managers/monitor_assignments/monitor_assignments_lib.php
@@ -19,7 +19,7 @@
  *
  * @author     Jeison Cardona Gómez
  * @package    block_ases
- * @copyright  2018 Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
+ * @copyright  2019 Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require_once(dirname(__FILE__). '/../../../../config.php');
