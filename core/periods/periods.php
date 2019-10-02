5486b25b1cae43af8ebb1e3e8fbfb70cdde09dc4
vvbv <jeison.cardona@correounivalle.edu.co>
2019-10-02 14:05:46 -0500

    Added periods_get_all_periods

--- core/periods/periods.php
+++ core/periods/periods.php
@@ -16,7 +16,8 @@ global $PERIODS_TABLENAME;
 $PERIODS_TABLENAME = $GLOBALS[ 'CFG' ]->prefix . "talentospilos_semestre";
 require_once( __DIR__ . "/v" . VERSION . "/entrypoint.php");
 
-/* Interface to periods_get_current_period
+/**
+ * Interface to periods_get_current_period
  * 
  * @author Jeison Cardona Gomez <jeison.cardona@correounivalle.edu.co>
  * @since 1.0.0
@@ -28,7 +29,8 @@ function core_periods_get_current_period(){
     return periods_get_current_period();
 }
 
-/* Interface to periods_get_period_by_id
+/**
+ * Interface to periods_get_period_by_id
  * 
  * @author Jeison Cardona Gomez <jeison.cardona@correounivalle.edu.co>
  * @since 1.0.0
@@ -43,6 +45,20 @@ function core_periods_get_current_period(){
  */
 function core_periods_get_period_by_id( int $period_id ){
     return periods_get_period_by_id( $period_id );
+}
+
+/**
+ * Interface to periods_get_all_periods
+ * 
+ * @author Jeison Cardona Gomez <jeison.cardona@correounivalle.edu.co>
+ * @since 1.0.0
+ * 
+ * @see periods_get_all_periods(...) in entrypoint.php
+ * 
+ * @return array List of periods.
+ */
+function core_periods_get_all_periods(){
+    return periods_get_all_periods();
 }
 
 ?>
