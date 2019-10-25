c257d3e6764005fb373a7805a8a7f3aa9cf506e1
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2019-10-25 16:03:07 -0500

    bootstrap definition removed

--- amd/src/geographic_main.js
+++ amd/src/geographic_main.js
@@ -8,7 +8,7 @@
  * @license  http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */ 
 
-define(['jquery', 'block_ases/bootstrap', 'block_ases/sweetalert', 'block_ases/jqueryui'], function($, bootstrap, sweetalert, jqueryui) {
+define(['jquery', 'block_ases/sweetalert', 'block_ases/jqueryui'], function($, sweetalert, jqueryui) {
 
     return {
 
