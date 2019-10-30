9bd0fa5af3ada889d095592cc95cb4fbb3fbdef1
vvbv <jeison.cardona@correounivalle.edu.co>
2019-10-30 15:31:07 -0500

    Added special case.

--- core/dphpforms/v1/DOMTools.php
+++ core/dphpforms/v1/DOMTools.php
@@ -2,12 +2,18 @@
 
 require_once( __DIR__ . '/classes/DOMAttributeList.php' );
 
-function _dom_add_attributtes( DOMElement &$tag, DOMAttributeList $attr_obj ):void
+function _core_dphpforms_dom_add_attributtes( DOMElement &$tag, DOMAttributeList $attr_obj ):void
 {   
     foreach( $attr_obj->getAttributes() as $attr => $val ){
         
         $is_array = ( ( gettype( array() ) == gettype( $val ) ) ? true : false );
-        $tag->setAttribute( $attr, ($is_array ? implode( " " , $val) : $val ) );
+        if( ( ( $attr == "disabled" ) || ( $attr == "required" ) ) && !$val[0] ){
+            continue;
+        }else{
+            $val = "";
+        }
+        $tag->setAttribute( $attr, ($is_array ? implode( " " , array_filter($val)) : $val) );
+        
         
     }
 }
