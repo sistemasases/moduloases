f15d6e3e740212c2ca919612f718d6665a405548
vvbv <jeison.cardona@correounivalle.edu.co>
2019-11-18 14:19:43 -0500

    Checkpoint

--- core/dphpforms/v1/entrypoint.php
+++ core/dphpforms/v1/entrypoint.php
@@ -941,8 +941,62 @@ function dphpformsV2_generate_html_recorder( $id_form, $rol_, $initial_config = 
 }
 
 function dphpformsV2_generate_TEXTFIELD_LIST( &$dom, $id_formulario_pregunta, $context, $statement, $prefix_uniqid ){
-            
-    $div = _core_dphpforms_build_tag( $dom, "div", new DOMAttributeList() );
+    
+    $div_attr = new DOMAttributeList( [
+        'class' =>  [ 
+            "div-$id_formulario_pregunta", 
+            "dphpf-list", 
+            $context[ 'attr_class' ] ,
+            $context[ 'attr_local_alias' ] 
+        ],
+        'data-uid' => uniqid( $prefix_uniqid, true )
+    ] );
+    
+    $inner_element_attr = [
+        'class' =>  [ $context[ 'attr_inputclass' ] ],
+        'disabled' => $context[ 'enabled' ],
+        'required' => $context[ 'attr_required' ],
+        'type' => "text"
+    ];
+    
+    $elements = $context['options']->initial_list;
+    
+    $elements_pos = array();
+    foreach ($elements as $key => $_row){
+        $elements_pos[$key] = $_row->pos;
+    }
+    array_multisort($elements_pos, SORT_ASC, $elements);
+
+    $div = _core_dphpforms_build_tag( $dom, "div", $div_attr );
+    
+    $statem = _core_dphpforms_build_tag( $dom, "label" );
+    $statem->nodeValue = $statement . ":";
+    
+    $div->appendChild( $statem );
+
+    foreach( $elements as $key => $element ){
+        
+        $inner_element_attr['name'] = $id_formulario_pregunta . "_" . $element->id . "[]";
+        
+        $dom_element_attr = new DOMAttributeList( $inner_element_attr );
+        
+        $elem_container = _core_dphpforms_build_tag( $dom, "div", new DOMAttributeList([
+            "class" => [ "dphpf-list-element" ]
+        ]) );
+        
+        $label = _core_dphpforms_build_tag( $dom, "label", new DOMAttributeList() );
+        $label->nodeValue = $element->statement . ":";
+        
+        $br = _core_dphpforms_build_tag( $dom, "br", new DOMAttributeList() );
+        
+        $textfield = _core_dphpforms_build_tag( $dom, "input", $dom_element_attr) ;
+        
+        $elem_container->appendChild( $label );
+        $elem_container->appendChild( $br );
+        $elem_container->appendChild( $textfield );
+        
+        $div->appendChild( $elem_container );
+    }
     
     return $div;
 
