b89a452a61ab8a2bdbb56ddaf1e25dae3fa7b639
vvbv <jeison.cardona@correounivalle.edu.co>
2020-01-15 23:01:54 -0500

    minor

--- core/dphpforms/test_old.php
+++ core/dphpforms/test_old.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require( "/usr/local/www/apache24/data/moodle35/blocks/ases/managers/dphpforms/dphpforms_response_recorder.php" );
+require( "/var/www/html/moodle366/blocks/ases/managers/dphpforms/dphpforms_response_recorder.php" );
+require( "/var/www/html/moodle366/blocks/ases/managers/dphpforms/dphpforms_record_updater.php" );
 
-echo dphpforms_generate_html_recorder( 'seguimiento_pares', "sistemas", -1, -1  );
+//echo dphpforms_generate_html_recorder( 'seguimiento_pares', "sistemas", -1, -1  );
+echo dphpforms_generate_html_updater( 1, "sistemas", 50115 );
