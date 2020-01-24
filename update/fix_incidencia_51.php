683b7409531d5562e94aec105c6285241cd8f777
Jorgemayor <mayor.jorge@correounivalle.edu.co>
2020-01-24 11:18:54 -0500

    Testing comments added

--- update/fix_incidencia_51.php
+++ update/fix_incidencia_51.php
@@ -18,9 +18,22 @@ $user_to_deprecate = new stdClass();
 $user_to_deprecate->id = $id_ases_deprecated;
 $user_to_deprecate->num_doc_ini = 939393;
 $user_to_deprecate->num_doc = 939393;
-$DB->update_record("talentospilos_usuario", $user_to_deprecate);
 
-$DB->delete_records('talentospilos_user_extended', ['id_ases_user' => $id_ases_deprecated, 'id_moodle_user' => $id_moodle_with_two_relations]);
+$result_update = $DB->update_record("talentospilos_usuario", $user_to_deprecate);
+
+if ($result_update) {
+    echo "Update succed";
+} else {
+    echo "Update failed";
+}
+
+$result_delete = $DB->delete_records('talentospilos_user_extended', ['id_ases_user' => $id_ases_deprecated, 'id_moodle_user' => $id_moodle_with_two_relations]);
+
+if ($result_delete) {
+    echo "Delete succed";
+} else {
+    echo "Delete failed";
+}
 
 $relation_to_create = new stdClass();
 $relation_to_create->id = $highest_id+1;
@@ -29,4 +42,11 @@ $relation_to_create->id_moodle_user = $id_moodle_without_relations;
 $relation_to_create->id_academic_program = $id_program_3492;
 $relation_to_create->tracking_status = 0;
 $relation_to_create->program_status = 4;
-$DB->insert_record("talentospilos_user_extended", $relation_to_create, true);
+
+$result_insert = $DB->insert_record("talentospilos_user_extended", $relation_to_create, true);
+
+if ($result_insert) {
+    echo "Insert succed";
+} else {
+    echo "Insert failed";
+}
