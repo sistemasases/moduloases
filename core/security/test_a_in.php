cebaa0f7aff1dc21870936e47928041e7aeca0e5
vvbv <jeison.cardona@correounivalle.edu.co>
2020-02-03 15:42:12 -0500

    minor: test

---
+++ core/security/test_a_in.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+require_once( __DIR__ . "/../module_loader.php" );
+
+module_loader( "security" );
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Al realizar una prueba que asinge o actualice el rol a un usuario utilizando un intervalo alternativo
+ * el que exista o no la tabla que se referencia dentro del intervalo alternativo no impide que se inserte
+ * una nueva fila en mdl_talentospilos_usuario_rol
+ * 
+ * Para replicar:
+ */
+$singularization_test_user = [ "id_instancia" => 563336, "id_semestre" => 3 ];
+
+$alt_interval = '{
+    
+    "col_name_interval_start" : "fecha_inicio",
+    "col_name_interval_end"   : "fecha_fin",
+    "table_ref"               : {
+    
+        "record_id" :1,
+        "name"      :"mdl_talentospilos_semestre"
+        
+    }
+}';
+ 
+print_r(_core_security_solve_alternative_interval($alt_interval));
+print_r(
+        core_secure_assign_role_to_user(
+                161037,
+                'sistemas',
+                strtotime("2020-01-28 09:00:00"),
+                strtotime("2020-02-29 00:00:00"),
+                $singularization_test_user,
+                True,
+                $alt_interval
+        )
+);
