105dd2edf049d3bb986a90ca2e0b9dbfcead9dc0
rhazek12 <sezaru@hotmail.es>
2020-12-13 12:54:19 -0500

    Validaciones corregidas

--- managers/student_profile/geographic_api.php
+++ managers/student_profile/geographic_api.php
@@ -72,11 +72,11 @@ if(isset($input->function) && isset($input->params)) {
             $nivel_riesgo = (int) $nivel_riesgo;
 
             //Validations of each parameter
-            if (is_string($id_ases) && is_float($latitude) && is_float($longitude) &&
-                is_string($neighborhood) && is_int($duration) && is_int($distance) &&
-                is_string($address) && is_string($city) && is_string($observaciones) &&
-                is_bool($vive_lejos) && is_bool($vive_zona_riesgo) && is_bool($nativo) &&
-                is_int($nivel_riesgo)) {
+            if(isset($id_ases) && is_string($id_ases) &&  is_numeric($latitude) && 
+                is_numeric($longitude) && is_string($neighborhood) && is_int($duration) && 
+                is_int($distance) && is_string($address) && is_string($city) && 
+                is_string($observaciones) && is_bool($vive_lejos) && is_bool($vive_zona_riesgo) && 
+                is_bool($nativo) && is_int($nivel_riesgo)) {
 
                 $vive_lejos = ($vive_lejos) ? 1 : 0;
                 $vive_zona_riesgo = ($vive_zona_riesgo) ? 1 : 0;
