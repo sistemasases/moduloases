3ffc004f4f9aed5e667909ac7dbd73d60b19140c
rhazek12 <sezaru@hotmail.es>
2021-02-24 02:08:11 -0500

    modificacion en substr

--- view/student_profile.php
+++ view/student_profile.php
@@ -207,7 +207,7 @@ if ($student_code != 0) {
         foreach($student_codes as $codes){
             $moodle_username = $codes->code;
             file_put_contents("../test.txt","codigo: ".$moodle_username,FILE_APPEND);
-            $student_program = substr($moodle_username,8,12);
+            $student_program = substr($moodle_username,-4);
             if($cod_programa == $student_program){
                 $cod_programa = $moodle_username;
                 file_put_contents("../test.txt","codigo dentro del if: ".$cod_programa."//",FILE_APPEND);
