30738f18ff5506aba22eec22ffbee0e4d77242f8
SebastianBetancourt <joan.betancourt@correounivalle.edu.co>
2021-03-26 10:29:24 -0500

    view monitorias académicas: inscripción añadida

--- managers/asistencia_monitorias/asistencia_monitorias_lib.php
+++ managers/asistencia_monitorias/asistencia_monitorias_lib.php
@@ -34,9 +34,9 @@ function get_tabla_monitorias($course_id, $block_id){
     array_push($columns, array("title"=>"Materia", "name"=>"materia", "data"=>"materia", "width"=>"18%"));
     array_push($columns, array("title"=>"Dia", "name"=>"dia", "data"=>"dia", "width"=>"10%"));
     array_push($columns, array("title"=>"Hora", "name"=>"hora", "data"=>"hora", "width"=>"12%"));
-    array_push($columns, array("title"=>"Nombre Monitor", "name"=>"firstname_mon", "data"=>"firstname_mon", "width"=>"20%"));
-    array_push($columns, array("title"=>"Apellido Monitor", "name"=>"lastname_mon", "data"=>"lastname_mon", "width"=>"20%"));
-    array_push($columns, array("title"=>"", "name"=>"botones", "data"=>"botones", "width"=>"20%", 
+    array_push($columns, array("title"=>"Nombre Monitor", "name"=>"firstname_mon", "data"=>"firstname_mon", "width"=>"18%"));
+    array_push($columns, array("title"=>"Apellido Monitor", "name"=>"lastname_mon", "data"=>"lastname_mon", "width"=>"18%"));
+    array_push($columns, array("title"=>"", "name"=>"botones", "data"=>"botones", "width"=>"24%", 
     "defaultContent"=>
     "<button class=\"dt-button buttons-print\">Ver sesiones programadas</button>
     <button class=\"dt-button buttons-print\">Eliminar</button>"));
@@ -240,9 +240,6 @@ function programar_sesiones($id_monitoria, $dia, $desde, $hasta){
     $siguiente_dia = $desde;
     // mientras $siguiente_dia siga estando antes de $hasta, programar sesión
     while(intval($siguiente_dia->format("Ymd")) <= $hasta){
-        // correr $siguiente_dia hasta el proximo $dia (método modify de php hace la magia)
-        $siguiente_dia->modify('next day');
-        $siguiente_dia->modify($dia_ingles);
         $fecha =  intval($siguiente_dia->format("Ymd"));
         if(count($DB->get_records_sql("SELECT 1
                     FROM {talentospilos_sesi_monitoria} sesion 
@@ -257,6 +254,9 @@ function programar_sesiones($id_monitoria, $dia, $desde, $hasta){
             $sesion->eliminado = false;
             $DB->insert_record('talentospilos_sesi_monitoria', $sesion, $returnid=false, $bulk=false);
         }
+        // correr $siguiente_dia hasta el proximo $dia (método modify de php hace la magia)
+        $siguiente_dia->modify('next day');
+        $siguiente_dia->modify($dia_ingles);
     }
 }
 
@@ -453,4 +453,138 @@ function formatear_fecha_int_a_legible(int $fecha){
     $MMM = array("ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic")[intval(substr($cadena, 4, 2))-1];
     $dd = substr($cadena, 6, 2);
     return $dd."/".$MMM."/".$yy;
+}
+
+function get_monitorias_para_inscribirse(){
+    global $DB;
+    $sql_query = "SELECT monitoria.id AS id, 
+                        dia, 
+                        hora, 
+                        materia.nombre AS materia 
+                FROM {talentospilos_monitoria} monitoria
+                    INNER JOIN {talentospilos_mate_monitoria} materia
+                    ON monitoria.materia = materia.id
+                WHERE monitoria.eliminado IS DISTINCT FROM 1
+                ORDER BY dia ASC";
+   return $DB->get_records_sql($sql_query);
+}
+
+function get_proxima_sesion_de_monitoria($monitoria_id){
+    global $DB;
+    $hoy = (new DateTime())->format("Ymd");
+    $sql = "SELECT  sesion.id AS id, 
+                    sesion.id_monitoria AS monitoria,
+                    sesion.fecha AS fecha 
+            FROM {talentospilos_sesi_monitoria} sesion 
+            WHERE 
+            sesion.id_monitoria = $monitoria_id AND
+             sesion.eliminado IS DISTINCT FROM 1
+                AND sesion.fecha >= $hoy
+            ORDER BY fecha ASC 
+            LIMIT 2";
+            // sesion.id_monitoria = $monitoria_id AND
+    $sesiones = array_values($DB->get_records_sql($sql));
+    $proxima_sesion = $sesiones[0];
+    if($sesiones[0]->fecha == $hoy){
+        // MANEJO ESPECIAL CUANDO LA SESION ES EL MISMO DÍA QUE EL USUARIO SE VA REGISTRAR
+        // El objetivo de este bloque de codigo es determinar si la hora final de la monitoría ya pasó. Si ya pasó, devolver la siguiente sesión programada
+
+        $hora_final = array_slice(explode(' ',$DB->get_field('talentospilos_monitoria', 'hora', ['id' => $monitoria_id])), 3, 2);
+        // Si la monitoría era en la mañana (AM) y en este instante ya estamos en la tarde (PM), retornar la siguiente sesion
+        if($hora_final[1] == 'AM' && (new DateTime())->format("A") == 'PM') $proxima_sesion =  $sesiones[1];
+        // Si la monitoría es en la tarde y todavía estamos en la mañana, retornar la sesión de hoy
+        else if($hora_final[1] == 'PM' && (new DateTime())->format("A") == 'AM') $proxima_sesion =  $sesiones[0];
+        // Si estamos en la misma jornada, comparar las horas como si fueran numeros. (ej, si la monitoría termina a las 4:00 y son las 2:03, se hace la comparación 400 > 203)
+        else $proxima_sesion =  intval(str_replace(':','',$hora_final[0])) > intval((new DateTime())->format("gi")) ? $sesiones[0] : $sesiones[1];
+        
+    }
+    $proxima_sesion->fecha_legible = formatear_fecha_int_a_legible($proxima_sesion->fecha);
+    return $proxima_sesion;
+}
+
+function anadir_asistente_a_sesion_de_monitoria($sesion, $id_asistente, $asignatura_a_consultar, $tematica_a_consultar){
+    global $DB;
+    $asistente = new stdClass();
+    $asistente->sesion = $sesion;
+    // talvez me equivoqué nombrando este campo como "asistente". 
+    // Este campo es una llave foranea a mdl_user, es el id del usuario de moodle que va a asistir a la monitoria
+    $asistente->asistente = $id_asistente;
+    $asistente->fecha_inscripcion = (new DateTime())->format("Ymd"); // hoy, en el formato custom especificado arriba
+    $asistente->asignatura_a_consultar = $asignatura_a_consultar;
+    $asistente->tematica_a_consultar = $tematica_a_consultar;
+    $asistente->asiste = false;
+    $asistente->eliminado = false;
+
+    return $DB->insert_record('talentospilos_asis_monitoria', $asistente, $returnid=false, $bulk=false);    
+}
+
+function cargar_inscripciones_de_usuario($id_usuario){
+    global $DB;
+    $hoy = (new DateTime())->format("Ymd");
+    $sql =    "SELECT
+                   inscripcion.id AS id,
+                   monitoria.dia AS dia_numero,
+                   sesion.fecha AS fecha, 
+                   monitoria.hora AS horario,
+                   materia.nombre AS materia, 
+                   monitor_encargado.firstname AS nombre_encargado, 
+                   monitor_encargado.lastname AS apellido_encargado,
+                   monitor_encargado.email AS correo_encargado,
+                   inscripcion.asignatura_a_consultar AS asignatura,
+                   inscripcion.tematica_a_consultar AS tematica
+                FROM {talentospilos_asis_monitoria} inscripcion
+                    INNER JOIN {talentospilos_sesi_monitoria} sesion
+                    ON inscripcion.sesion = sesion.id
+                        INNER JOIN {talentospilos_monitoria} monitoria
+                        ON sesion.id_monitoria = monitoria.id
+                            INNER JOIN {talentospilos_mate_monitoria} materia
+                            ON monitoria.materia = materia.id
+                                INNER JOIN {user} monitor_encargado
+                                ON monitoria.monitor = monitor_encargado.id
+                WHERE 
+                    inscripcion.asistente = $id_usuario 
+                    AND (inscripcion.eliminado IS DISTINCT FROM 1)
+                    AND (sesion.eliminado IS DISTINCT FROM 1)
+                    AND (monitoria.eliminado IS DISTINCT FROM 1)
+                    AND (sesion.fecha >= $hoy)
+                ORDER BY sesion.fecha ASC";
+    
+    $results = $DB->get_records_sql($sql);
+    error_log(var_export($results, true));
+    foreach($results as $result) {
+        $result->dia = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")[$result->dia_numero];
+        $result->fecha = formatear_fecha_int_a_legible($result->fecha);
+    }
+    return $results;
+}
+
+function eliminar_asistencia($id){
+    global $DB;
+    $sql = "SELECT * FROM {talentospilos_asis_monitoria} WHERE id = '$id'";
+    $asistencia = $DB->get_record_sql($sql);
+    if($asistencia){
+        $asistencia->eliminado = true;
+        if($asistencia->id == 0){
+            trigger_error('ASES Notificacion: actualizar monitoria en la BD con id 0');
+            return -1;
+        }
+        $DB->update_record('talentospilos_asis_monitoria', $asistencia, $bulk=false);
+        return 1;
+    }else{
+        return -1;
+    }
+}
+
+function modificar_celular_de_usuario($id, $celular){
+    global $DB;
+    $user = $DB->get_record_sql("SELECT * FROM {user} WHERE id = $id");
+    if($user){
+        $user->phone1 = $celular;
+        if($user->id == 0){
+            trigger_error('ASES Notificacion: actualizar usuario en la BD con id 0');
+            return -1;
+        }
+        //error_log(var_export($user, true));
+        return $DB->update_record('user', $user, $bulk=false);
+    } 
 }
