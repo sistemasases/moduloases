07c59fba97956cbce7dd30ab7434a8136890d150
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-05-27 16:02:49 -0500

    Script para añadir instancia a ciertos periodos.

--- version.php
+++ version.php
@@ -4,10 +4,8 @@
     /**
      * PARA ARREGLAR VERSIÓN EN PRODUCCIÓN
      * UPDATE mdl_config_plugins SET value=2021032313380 WHERE id=1659;
-     * 
-     * También proveo un script que hace lo mismo, para mayor
-     * comodidad desde el cli: php fix_version.php 
-     * con php 7.4
+     *
+     * Debe hacerse antes de realizar cualquier cosa. 
      * 
      * David S. Cortés
      */
