f0c62762343cab9d5bcb18920b9ee1a0d7feacc5
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-06-16 20:27:59 -0500

    Correción llamados a send_email_dphpforms y solución de algunos llamados a get_assigned_monitor, get_assigned_pract y get_assigned_profesional. Más pendientes.

--- managers/get_info_psicosocial.php
+++ managers/get_info_psicosocial.php
@@ -4,11 +4,11 @@ require_once('query.php');
 
 if(isset($_POST['fun'])){
     if($_POST['fun'] == 'get_professional'){
-        echo get_assigned_professional($_POST['idStudent']);    
+        echo get_assigned_professional($_POST['idStudent'], $_POST['instance']);    
     }else if($_POST['fun'] == 'get_practicante'){
-        echo get_assigned_pract($_POST['idStudent']);
+        echo get_assigned_pract($_POST['idStudent'], $_POST['instance']);
     }else if($_POST['fun'] == 'get_monitor'){
-        echo get_assigned_monitor($_POST['idStudent']);
+        echo get_assigned_monitor($_POST['idStudent'], $_POST['instance']);
     }
 }
 
