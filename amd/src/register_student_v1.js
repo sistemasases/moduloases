e3ef71453d2c761e9cdb1f2088e5ebf49879e2ea
ASES Dev <sistemas.ases@correounivalle.edu.co>
2022-12-19 10:30:47 -0600

    Ajustes Discapacidad e Incluisión (#2309)
    * Mejorar mensaje del modal.
    
    * Habilitar modal_register_student
    
    * Fixed error Dylan
    
    * estilo en-linea porque hay muchas reglas sobre el elemento.
    
    * se optimizo una consulta de la base de datos, partiendo de la consulta de antes y de que ahora ya no es necesaria la relacion con las tablas de ases y solucionado depaso las cosas raras que pasaban con la consulta anterior
    
    * Revert "Merge pull request #2301 from tsubakijuju13/tsubaki_0x01"
    
    This reverts commit 4ae4cd58195d29a2079b4f3b02281c231c6ca92f, reversing
    changes made to 353eef02532e2d2e324c2862d1b9d1efa7145aaf. Mientras que
    se añade el .min y probamos en el server.
    
    * Revision .min
    
    * To David
    
    * Changes to review or revert
    
    * css to view register
    
    * Revisiones minimas
    
    * Update 4 and 5 sections
    
    * nuevas configuraciones
    
    * Minimal changes
    
    * Renderizado de Side_menu y minificado de js
    
    Co-authored-by: Santiago Cortés <david.cortes@correounivalle.edu.co>
    Co-authored-by: tsubakijuju13 <santiago.ramirez.ospina@correounivalle.edu.co>
    Co-authored-by: つばき_0x01 <60707399+tsubakijuju13@users.noreply.github.com>
    Co-authored-by: Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
    Co-authored-by: tsubaki_0x01 <tsubaki_0x01@pop-os.localdomain>

---
+++ amd/src/register_student_v1.js
@@ -0,0 +1,17 @@
+/**
+ * #Short description
+ *
+ * @module    amd/src/#module_name
+ * @author    #author
+ * @copyright #year #author #email
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define([jquery], function($){
+
+    return {
+        init: function () {
+			
+        }
+    }
+});
